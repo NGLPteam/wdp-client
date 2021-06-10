cf3e6a1596d3d311f5bfd399bfc3dc4ad4d4646f
Amber Mitchell <amber@castironcoding.com>
2021-06-10 08:09:51 -0700

    [C] Update page name from task to view

--- pages/[entity]/[id]/manage/[[...view]].tsx
+++ pages/[entity]/[id]/manage/[[...view]].tsx
@@ -4,12 +4,12 @@ import useGetQueryParam from "hooks/useGetQueryParam";
 export default function EntityManagePage() {
     const entity = useGetQueryParam("entity");
     const id = useGetQueryParam("id");
-    const task = useGetQueryParam("task") || "properties";
+    const view = useGetQueryParam("view") || "properties";
 
     return (
         <div>
             <h1>
-                {entity} {id} - Manage {task}
+                {entity} {id} - Manage {view}
             </h1>
         </div>
     );
