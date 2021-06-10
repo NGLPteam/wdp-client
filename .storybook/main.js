5cd6b4c8a18ab72712874c9eb69de4a9586b242c
jen-castiron <jen@castironcoding.com>
2021-06-10 09:48:52 -0700

    [F] Add Storybook - Resolves #WDP-30 - Add support for typescript

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../components/**/*.stories.tsx"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/components": path.resolve(__dirname, "../components"),
+      "@/theme": path.resolve(__dirname, "../theme"),
+    };
+
+    return config;
+  },
+};
