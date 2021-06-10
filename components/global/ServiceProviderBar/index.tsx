5d65e7f0e8369c5247c3e2bb242b5483aa579902
jen-castiron <jen@castironcoding.com>
2021-06-10 14:56:47 -0700

    [F] Add container margins

--- components/global/ServiceProviderBar/index.tsx
+++ components/global/ServiceProviderBar/index.tsx
@@ -14,7 +14,7 @@ const StyledServiceProviderBar = styled(ServiceProviderBar)`
   margin: 0 auto;
   width: 100%;
   background: var(--brand90);
-  padding: 12px;
+  padding: 12px var(--container-column-margin);
   color: var(--brand20);
   text-align: center;
 `;
