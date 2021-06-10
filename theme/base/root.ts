1db462b1b5d14250f4c5428683b2c68ee9e7b8ac
jen-castiron <jen@castironcoding.com>
2021-06-10 10:30:55 -0700

    [F] Add label typography vars and utility classes

--- theme/base/root.ts
+++ theme/base/root.ts
@@ -41,10 +41,22 @@ export default `
     /* font weights */
     --font-weight-regular: 400;
     --font-weight-medium: 500;
+    --font-weight-semibold: 600;
     --font-weight-bold: 700;
 
     /* font sizes */
     --font-size-base: 1rem;
+    --font-size-sm: ${pxToRem("14px")};
+    
+    /* label font sizes */
+    --font-size-label-lg: ${pxToRem("15px")};
+    --line-height-label-lg: ${pxToRem("24px")};
+    --font-size-label-md: ${pxToRem("14px")};
+    --line-height-label-md: ${pxToRem("20px")};
+    --font-size-label-sm: ${pxToRem("13px")};
+    --line-height-label-sm: ${pxToRem("20px")};
+    
+    /* header font sizes */
     --font-size-h1: ${pxToRem("30px")};
     --line-height-h1: ${pxToRem("36px")};
     --font-size-h2: ${pxToRem("22px")};
