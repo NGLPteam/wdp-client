ed95162016f083e06e1ef0b2c92999d34b484af7
jen-castiron <jen@castironcoding.com>
2021-06-16 12:29:00 -0700

    [C] Add eslint-config-next

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Warning: Dangerously allow production builds to successfully complete even if
+  //   your project has ESLint errors.
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+};
