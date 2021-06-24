7f1a04515bbbbc034fad18df015e68bfa1d78008
Amber Mitchell <amber@castironcoding.com>
2021-06-24 13:12:36 -0700

    [C] Fix ts lint

--- pages/[entity]/index.tsx
+++ pages/[entity]/index.tsx
@@ -5,7 +5,6 @@ import CommunityListView from "components/views/communities/CommunityList";
 import ContributorListView from "components/views/contributors/ContributorList";
 import ItemListView from "components/views/items/ItemList";
 import UserListView from "components/views/users/UserList";
-import ContributorListView from "components/views/contributors/ContributorList";
 import PageNotFoundView from "components/views/PageNotFound";
 
 export default function EntityListPage({ ...pageProps }) {
@@ -17,7 +16,6 @@ export default function EntityListPage({ ...pageProps }) {
     contributors: ContributorListView,
     items: ItemListView,
     users: UserListView,
-    contributors: ContributorListView,
   };
 
   const Template = viewMap[entity] || PageNotFoundView;
