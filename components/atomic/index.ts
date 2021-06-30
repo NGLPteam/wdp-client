9cb9a20c0202a2e003c702a3b031d61cb51d9a42
jen-castiron <jen@castironcoding.com>
2021-06-30 12:05:01 -0700

    [F] Style top nav dropdown

--- components/atomic/index.ts
+++ components/atomic/index.ts
@@ -1,5 +1,6 @@
 export { default as MixedLink } from "./MixedLink";
 export { default as TopNavItem } from "./TopNavItem";
+export { default as TopNavDropdown } from "./TopNavDropdown";
 export { default as TabNav } from "./TabNav";
 export { default as ContentTitle } from "./ContentTitle";
 export { default as Breadcrumbs } from "./Breadcrumbs";
