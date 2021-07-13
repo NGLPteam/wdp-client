273689437905768b056af441421bbd66a4fe00dc
Zach Davis <zach@castironcoding.com>
2021-07-13 07:02:56 -0700

    [C] Remove scaffolded HP content

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,31 +5,5 @@ import Link from "next/link";
 export default function Home() {
   useSetActiveEntity();
 
-  return (
-    <div>
-      <h1>NGLP Admin</h1>
-      <ul>
-        <li>
-          <Link href="/collections">
-            <a>Collections</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/communities">
-            <a>Communities</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/items">
-            <a>Items</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/users">
-            <a>Users</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div>WDP Admin Homepage</div>;
 }
