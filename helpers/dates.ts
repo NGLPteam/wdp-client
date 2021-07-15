718817d8560ae4b8b038cab41f80b87d7780db31
Zach Davis <zach@castironcoding.com>
2021-07-15 15:05:11 -0700

    [B] Use 24 hour format to prevent date exception

--- helpers/dates.ts
+++ helpers/dates.ts
@@ -3,6 +3,6 @@ import parse from "date-fns/parse";
 
 /** Formats a date from the backend, default is `PPp`, ie `MMM d, yyyy, h:mm a`  */
 export function formatDate(dateString: string, formatString?: string) {
-  const parsedDate = parse(dateString, `yyyy-MM-dd'T'hh:mm:ss'Z'`, new Date());
+  const parsedDate = parse(dateString, `yyyy-MM-dd'T'kk:mm:ss'Z'`, new Date());
   return format(parsedDate, formatString || "PPp");
 }
