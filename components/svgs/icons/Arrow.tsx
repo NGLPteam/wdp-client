a3218c03afae614a419d2161fd832f5158386ea0
jen-castiron <jen@castironcoding.com>
2021-07-15 15:11:32 -0700

    [F] Add Checkbox icon, fix size prop on Icons

--- components/svgs/icons/Arrow.tsx
+++ components/svgs/icons/Arrow.tsx
@@ -3,8 +3,6 @@ import Props from "./iconType";
 function IconArrow({ title, ...props }: Props) {
   return (
     <svg
-      width="16"
-      height="16"
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
---
+++ components/svgs/icons/Checkbox.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Props from "./iconType";
+
+const IconCheckbox = ({ title, fill, ...props }: CheckboxProps) => {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      {...props}
+    >
+      <rect
+        x=".5"
+        y=".5"
+        width="15"
+        height="15"
+        rx="3.5"
+        stroke="currentColor"
+        fill="none"
+        data-path-name="box"
+      />
+      <path
+        d="M3 8l4 3.5 6-7"
+        stroke="currentColor"
+        fill="none"
+        data-path-name="check"
+      />
+      <path d="M4 8H12" stroke="none" data-path-name="indeterminate" />
+    </svg>
+  );
+};
+
+interface CheckboxProps extends Props {
+  checked?: boolean;
+}
+
+export default IconCheckbox;
