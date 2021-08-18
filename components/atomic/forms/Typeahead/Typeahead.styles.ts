eac20422ed258cb44a673a0f9488c18eac7e4ed3
jen-castiron <jen@castironcoding.com>
2021-08-18 12:43:21 -0700

    [F] Build Typeahead input

---
+++ components/atomic/forms/Typeahead/Typeahead.styles.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { aBaseInput, basePadding } from "theme/mixins/appearance";
+import { pxToRem } from "theme/mixins/functions";
+import { IconFactory } from "components/factories";
+import { noInsetSupport } from "theme/mixins/base";
+import { tTruncate } from "theme/mixins/typography";
+
+export const InputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  --input-min-height: ${pxToRem("42px")};
+  --input-padding: 0 ${basePadding(4)};
+  --input-focus-background: var(--brand10);
+  width: 100%;
+  ${aBaseInput()}
+`;
+
+export const Icon = styled(IconFactory)``;
+
+export const Button = styled.button`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  inset-inline-end: 0;
+  inset-block-start: 0;
+  inset-block-end: 0;
+  padding-inline-start: ${basePadding(4)};
+  padding-inline-end: ${basePadding(4)};
+
+  ${noInsetSupport(`top: 0; right: 0; bottom: 0;`)}
+`;
+
+export const List = styled.ul<ListProps>`
+  position: absolute;
+  background-color: var(--input-focus-background, var(--brand10));
+  border: 1px solid var(--accent-color);
+  border-top: none;
+  box-shadow: 0px 12px 24px -12px rgba(0, 0, 0, 0.3);
+  inset-inline-start: 0;
+  inset-inline-end: 0;
+  inset-block-start: 0;
+  max-height: 50vh;
+  overflow-y: auto;
+  transform: translateY(${pxToRem("41px")});
+
+  ${noInsetSupport(`left: 0; right: 0; top: 0;`)}
+
+  ${({ isOpen }) => !isOpen && `opacity: 0;`}
+`;
+
+interface ListProps {
+  isOpen?: boolean;
+}
+
+export const ListItem = styled.li<ListItemProps>`
+  cursor: pointer;
+  padding-inline-start: ${basePadding(4)};
+  padding-inline-end: ${basePadding(4)};
+  padding-block-start: ${basePadding(3)};
+  padding-block-end: ${basePadding(3)};
+  ${tTruncate}
+
+  ${({ highlighted }) => highlighted && `background-color: var(--brand20)`}
+`;
+
+interface ListItemProps {
+  highlighted?: boolean;
+}
---
+++ components/atomic/forms/Typeahead/Typeahead.tsx
@@ -0,0 +1,119 @@
+import React, { Ref, forwardRef, useState } from "react";
+import { useCombobox } from "downshift";
+import BaseInputWrapper from "../BaseInputWrapper";
+import BaseInputLabel from "../BaseInputLabel";
+import * as Styled from "./Typeahead.styles";
+import BaseInputDescription from "../BaseInputDescription";
+
+import type InputProps from "../inputType";
+
+/**
+ * A typeahead single select.
+ * Uses `downshift` library for accessibility and state management
+ */
+const Typeahead = forwardRef(
+  (
+    {
+      label,
+      hideLabel,
+      description,
+      placeholder,
+      options,
+      required,
+      onChange,
+      value,
+      ...inputProps
+    }: Props,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const [inputOptions, setInputOptions] = useState(options);
+
+    const {
+      isOpen,
+      getToggleButtonProps,
+      getLabelProps,
+      getMenuProps,
+      getInputProps,
+      getComboboxProps,
+      highlightedIndex,
+      getItemProps,
+    } = useCombobox({
+      items: inputOptions,
+      itemToString: (item) => (item ? item.label : ""),
+      onInputValueChange: ({ inputValue }) => {
+        // If no value is provided, reset options to default
+        if (!inputValue) {
+          setInputOptions(options);
+          return;
+        }
+
+        // Filter options by current input value
+        setInputOptions(
+          options.filter((item) =>
+            item.label.toLowerCase().includes(inputValue.toLowerCase())
+          )
+        );
+      },
+      onSelectedItemChange: ({ selectedItem }) => {
+        // Send the current value to the parent onChange function
+        if (onChange) onChange(selectedItem?.value || "");
+      },
+    });
+
+    return (
+      <BaseInputWrapper>
+        <BaseInputLabel hideLabel={hideLabel} {...getLabelProps()}>
+          {label}
+        </BaseInputLabel>
+        <Styled.InputWrapper {...getComboboxProps()}>
+          {/* Hidden input field for react-hook-form or other form control */}
+          <input
+            ref={ref}
+            type="hidden"
+            required={required}
+            defaultValue={value}
+            {...inputProps}
+          />
+          {/* Visible input field for typeahead functionality */}
+          <Styled.Input {...getInputProps()} required={required} />
+          <Styled.Button
+            type="button"
+            {...getToggleButtonProps()}
+            aria-label="toggle menu"
+          >
+            <Styled.Icon icon="chevron" rotate={180} />
+          </Styled.Button>
+          <Styled.List {...getMenuProps()} isOpen={isOpen}>
+            {isOpen &&
+              inputOptions.map((item, index) => (
+                <Styled.ListItem
+                  highlighted={highlightedIndex === index}
+                  key={index}
+                  {...getItemProps({ key: item.value, index, item })}
+                >
+                  {item.label}
+                </Styled.ListItem>
+              ))}
+          </Styled.List>
+        </Styled.InputWrapper>
+        {description && (
+          <BaseInputDescription>{description}</BaseInputDescription>
+        )}
+      </BaseInputWrapper>
+    );
+  }
+);
+
+interface Option {
+  label: string;
+  value: string | number;
+}
+interface Props extends InputProps {
+  options: Option[];
+  /** Returns the current selected value */
+  onChange?: (value: Option["value"]) => void;
+  /** Default value */
+  value?: Option["value"];
+}
+
+export default Typeahead;
