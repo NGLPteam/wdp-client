3fdcdaaf8c68225ca2dd33cf990f3984c6b69e6b
Alexa Grey <alexa@castironcoding.com>
2021-08-25 03:27:33 -0700

    [C] Add SlugToID helper component
    This component can be used to turn entity slugs into node IDs, which
    are needed for internal mappings, passing to mutations, etc. They are
    designed to be very low-impact, and will benefit from relay caching.

---
+++ __generated__/SlugToIDCommunityQuery.graphql.ts
@@ -0,0 +1,93 @@
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from "relay-runtime";
+export type SlugToIDCommunityQueryVariables = {
+    slug: string;
+};
+export type SlugToIDCommunityQueryResponse = {
+    readonly community: {
+        readonly id: string;
+    } | null;
+};
+export type SlugToIDCommunityQuery = {
+    readonly response: SlugToIDCommunityQueryResponse;
+    readonly variables: SlugToIDCommunityQueryVariables;
+};
+
+
+
+/*
+query SlugToIDCommunityQuery(
+  $slug: Slug!
+) {
+  community(slug: $slug) {
+    id
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "slug"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": [
+      {
+        "kind": "Variable",
+        "name": "slug",
+        "variableName": "slug"
+      }
+    ],
+    "concreteType": "Community",
+    "kind": "LinkedField",
+    "name": "community",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "id",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "SlugToIDCommunityQuery",
+    "selections": (v1/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "SlugToIDCommunityQuery",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "edddfbcbfbb9c5815f59e83987a36e83",
+    "id": null,
+    "metadata": {},
+    "name": "SlugToIDCommunityQuery",
+    "operationKind": "query",
+    "text": "query SlugToIDCommunityQuery(\n  $slug: Slug!\n) {\n  community(slug: $slug) {\n    id\n  }\n}\n"
+  }
+};
+})();
+(node as any).hash = '37a1fe4fa3e7384878e17a3b9fb3f35d';
+export default node;
---
+++ components/api/SlugToID.tsx
@@ -0,0 +1,165 @@
+import React, { useCallback } from "react";
+import { graphql } from "react-relay";
+import type { GraphQLTaggedNode, OperationType } from "relay-runtime";
+
+import { SlugToIDCommunityQuery as CommunityQuery } from "@/relay/SlugToIDCommunityQuery.graphql";
+import { SlugToIDCollectionQuery as CollectionQuery } from "@/relay/SlugToIDCollectionQuery.graphql"; 
+import { SlugToIDItemQuery as ItemQuery } from "@/relay/SlugToIDItemQuery.graphql";
+
+import QueryWrapper from "./QueryWrapper";
+
+/**
+ * A component that will accept a community slug and extract its node id.
+ * 
+ * @see SlugToID
+ */
+export function CommunitySlugToID({ children, onEmpty, slug }: WrapperProps<CommunityQuery>) {
+  const extractId: IDExtractor<CommunityQuery> = useCallback((data) => data?.community?.id, []);
+
+  return (
+    <SlugToID<CommunityQuery>
+      children={children}
+      extractId={extractId}
+      query={communityQuery}
+      onEmpty={onEmpty}
+      variables={{ slug }}
+    />
+  );
+}
+
+/**
+ * A component that will accept a collection slug and extract its node id.
+ * 
+ * @see SlugToID
+ */
+export function CollectionSlugToID({ children, onEmpty, slug }: WrapperProps<CollectionQuery>) {
+  const extractId: IDExtractor<CollectionQuery> = useCallback((data) => data?.collection?.id, []);
+
+  return (
+    <SlugToID<CollectionQuery>
+      children={children}
+      extractId={extractId}
+      query={collectionQuery}
+      onEmpty={onEmpty}
+      variables={{ slug }}
+    />
+  );
+}
+
+/**
+ * A component that will accept an item slug and extract its node id.
+ * 
+ * @see SlugToID
+ */
+export function ItemSlugToID({ children, onEmpty, slug }: WrapperProps<ItemQuery>) {
+  const extractId: IDExtractor<ItemQuery> = useCallback((data) => data?.item?.id, []);
+
+  return (
+    <SlugToID<ItemQuery>
+      children={children}
+      extractId={extractId}
+      query={itemQuery}
+      onEmpty={onEmpty}
+      variables={{ slug }}
+    />
+  );
+}
+
+/**
+ * A specialized component that wraps a query accepting a slug, and transforms it into a Relay Node ID.
+ * If an ID is found, it will render its `children` function and provide the ID for consumption.
+ * 
+ * Optionally, an `onEmpty` function can be provided, else it will render nothing.
+ */
+export default function SlugToID<Q extends OperationType>({
+  children, extractId, onEmpty, query, variables
+}: SlugToIDProps<Q>) {
+  return (
+    <QueryWrapper<Q>
+      query={query}
+      initialVariables={variables}
+    >
+      {({ data }) => {
+        const id = extractId(data);
+
+        if (id) {
+          return children({ id, variables });
+        } else if (typeof onEmpty === "function") {
+          return onEmpty({ variables });
+        } else {
+          return null;
+        }
+      }}
+    </QueryWrapper>
+  );
+}
+
+/**
+ * A type describing a function that receives a GraphQL response and extracts what should be a Relay Node ID.
+ */
+type IDExtractor<Q extends OperationType> = (data: Q["response"]) => string | null | undefined;
+
+type RenderChildren<Q extends OperationType> = (props: RenderChildrenProps<Q>) => JSX.Element;
+
+type RenderEmpty<Q extends OperationType> = (props: RenderEmptyProps<Q>) => JSX.Element;
+
+interface RenderChildrenProps<Q extends OperationType> {
+  id: string;
+  variables: Q["variables"];
+};
+
+interface RenderEmptyProps<Q extends OperationType> {
+  variables: Q["variables"];
+}
+
+interface SlugToIDProps<Q extends OperationType> {
+  /**
+   * A `render props` function that will only be called if `extractId` returns an ID.
+   */
+  children: RenderChildren<Q>;
+  /**
+   * A function that receives a GraphQL response and extracts what should be a Relay Node ID, if present.
+   */
+  extractId: IDExtractor<Q>;
+  /**
+   * An optional function that renders something else if no ID was found. Otherwise, the component will return null.
+   */
+  onEmpty?: RenderEmpty<Q>;
+  /**
+   * A query that should resolve to a Relay Node ID
+   */
+  query: GraphQLTaggedNode;
+  /**
+   * Variables for the `query`
+   */
+  variables: Q["variables"];
+};
+
+interface WrapperProps<Q extends OperationType> {
+  children: RenderChildren<Q>;
+  slug: string;
+  onEmpty?: RenderEmpty<Q>
+};
+
+const communityQuery = graphql`
+query SlugToIDCommunityQuery($slug: Slug!) {
+  community(slug: $slug) {
+    id
+  }
+}`;
+
+const collectionQuery = graphql`
+query SlugToIDCollectionQuery($slug: Slug!) {
+  collection(slug: $slug) {
+    id
+  }
+}
+`;
+
+const itemQuery = graphql`
+query SlugToIDItemQuery($slug: Slug!) {
+  item(slug: $slug) {
+    id
+  }
+}
+`;
