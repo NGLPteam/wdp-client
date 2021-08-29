bcde44677b3a0667429853a2120fe1a21e69c85f
Zach Davis <zach@castironcoding.com>
2021-08-29 07:34:27 -0700

    [C] Remove unused vars

--- pages/contributors/[slug]/collections.tsx
+++ pages/contributors/[slug]/collections.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { useRouter } from "next/router";
 import { RouteHelper } from "routes";
 import ContributorLayout from "components/composed/contributor/ContributorLayout";
 import { Page } from "types/page";
 
 const ContributorCollectionContributions: Page = () => {
-  const router = useRouter();
   const activeRoute = RouteHelper.activeRoute();
 
   return <>{activeRoute?.label}</>;
