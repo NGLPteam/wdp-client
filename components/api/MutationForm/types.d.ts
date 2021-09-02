6c701cdfc65f2a6c66e4671089ae400b8b083405
Alexa Grey <alexa@castironcoding.com>
2021-09-02 10:29:01 -0700

    [F] Simplify MutationForm hooks / callbacks
    * Allow `getErrors` to be entirely optional. We now verify that the
      `name` passed to a MutationForm corresponds to a payload that has
      the MutationForm's error fragment included. We further allow this
      with a type predicate to make sure the response is the right shape
      at runtime.
    * Allow `toVariables` to *conditionally* be optional. If all a form's
      fields will satisfy the mutation's input type, we can simply return
      the form data without needing the hook. If the fields don't satisfy,
      then the component will mark `toVariables` as required.

--- components/api/MutationForm/types.d.ts
+++ components/api/MutationForm/types.d.ts
@@ -11,7 +11,7 @@ import type { MutationParameters } from "relay-runtime";
 
 import type { MutationForm_mutationErrors$key } from "@/relay/MutationForm_mutationErrors.graphql";
 
-export interface ErrorMap<T extends FieldValues = FieldValues> {
+export interface ErrorMap<T extends FieldValues> {
   attributes: readonly RemappedAttributeError<T>[];
   global: readonly string[];
   user: readonly string[];
@@ -27,17 +27,17 @@ export type HasErrorFragment = MutationForm_mutationErrors$key;
 
 export type IsSuccessPredicate<
   M extends MutationParameters,
-  T extends FieldValues = FieldValues
+  T extends FieldValues
 > = (response: M["response"], data: UnpackNestedValue<T>) => boolean;
 
 export type OnFailureCallback<
   M extends MutationParameters,
-  T extends FieldValues = FieldValues
+  T extends FieldValues
 > = (params: OnFailureResponse<M, T>) => void;
 
 interface OnFailureResponse<
   M extends MutationParameters,
-  T extends FieldValues = FieldValues
+  T extends FieldValues
 > {
   errors: ErrorMap<T>;
   response: M["response"];
@@ -47,21 +47,35 @@ interface OnFailureResponse<
 
 export type OnSuccessCallback<
   M extends MutationParameters,
-  T extends FieldValues = FieldValues
+  T extends FieldValues
 > = (params: OnSuccessResponse<M, T>) => void;
 
 interface OnSuccessResponse<
   M extends MutationParameters,
-  T extends FieldValues = FieldValues
+  T extends FieldValues
 > {
   response: M["response"];
   variables: M["variables"];
   values: UnpackNestedValue<T>;
 }
 
+/**
+ * A type that extracts the name(s) of mutation payload(s) from a Mutation response
+ * that have included `MutationForm_mutationErrors$key`. It's our way of ensuring that
+ * mutations have included that fragment, because it's vital to our ability to
+ * process errors in a uniform manner.
+ */
 export type MutationName<M extends MutationParameters> = {
-  [K in keyof M["response"]]-?: K & string;
+  [K in keyof M["response"]]-?: NonNullable<
+    M["response"][K]
+  > extends MutationForm_mutationErrors$key
+    ? K
+    : never;
 }[keyof M["response"]];
+
+export type PayloadWithErrors<
+  M extends MutationParameters
+> = M["response"][MutationName<M>] & MutationForm_mutationErrors$key;
 
 interface RemappedAttributeError<T> {
   path: Path<T>;
@@ -93,7 +107,52 @@ interface RenderFormProps<T extends FieldValues = FieldValues> {
   form: UseFormReturn<T>;
 }
 
+/**
+ * We often need to perform a little bit of transformation on the mutation
+ * input such as adding properties that aren't controlled by the form, removing
+ * null values, and so on.
+ *
+ * This function can be provided in order to accomplish just that. At the very
+ * least, you'll need to finesse the data into `input`.
+ *
+ * @see useToVariables to generate type-safely without extra renders
+ *
+ * @example <caption>Transforming Form Data Into Variables</caption>
+ * const toVariables = (data) => ({ input: data })
+ */
 export type VariableTransformer<
   M extends MutationParameters,
   T extends FieldValues = FieldValues
 > = (data: UnpackNestedValue<T>) => M["variables"];
+
+interface RequiresToVariables<
+  M extends MutationParameters,
+  T extends FieldValues = FieldValues
+> {
+  /**
+   * We often need to perform a little bit of transformation on the mutation
+   * input such as adding properties that aren't controlled by the form, removing
+   * null values, and so on.
+   *
+   * This function can be provided in order to accomplish just that. At the very
+   * least, you'll need to finesse the data into `input`.
+   *
+   * @see useToVariables to generate type-safely without extra renders
+   *
+   * @example <caption>Transforming Form Data Into Variables</caption>
+   * const toVariables = (data) => ({ input: data })
+   */
+  toVariables: VariableTransformer<M, T>;
+}
+
+type OptionalToVariables<
+  M extends MutationParameters,
+  T extends FieldValues = FieldValues
+> = Partial<RequiresToVariables<M, T>>;
+
+export type AcceptsToVariables<
+  M extends MutationParameters,
+  T extends FieldValues
+> = { input: T } extends M["variables"]
+  ? OptionalToVariables<M, T>
+  : RequiresToVariables<M, T>;
--- components/composed/contributor/ContributorCreateOrganizationForm/ContributorCreateOrganizationForm.tsx
+++ components/composed/contributor/ContributorCreateOrganizationForm/ContributorCreateOrganizationForm.tsx
@@ -3,9 +3,7 @@ import React from "react";
 import { graphql } from "react-relay";
 
 import MutationForm, {
-  useGetErrors,
   useRenderForm,
-  useToVariables,
   Forms,
 } from "components/api/MutationForm";
 
@@ -15,16 +13,6 @@ import type {
 } from "@/relay/ContributorCreateOrganizationFormMutation.graphql";
 
 export default function ContributorCreateOrganizationForm() {
-  const getErrors = useGetErrors<ContributorCreateOrganizationFormMutation>(
-    (response) => response.createOrganizationContributor ?? null,
-    []
-  );
-
-  const toVariables = useToVariables<
-    ContributorCreateOrganizationFormMutation,
-    Fields
-  >((data) => ({ input: data }), []);
-
   const renderForm = useRenderForm<Fields>(
     ({ form: { register } }) => (
       <Forms.Grid>
@@ -45,10 +33,8 @@ export default function ContributorCreateOrganizationForm() {
 
   return (
     <MutationForm<ContributorCreateOrganizationFormMutation, Fields>
-      getErrors={getErrors}
       mutation={mutation}
       name="createOrganizationContributor"
-      toVariables={toVariables}
     >
       {renderForm}
     </MutationForm>
