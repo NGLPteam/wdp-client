51158af5a8264dc98ec61d2cffa409ebf1981698
Alexa Grey <alexa@castironcoding.com>
2021-09-02 10:31:41 -0700

    [C] Add an `undefined`-safe useFragment hook
    To avoid extra ceremony where we want to pass `data?.foo` to the hook,
    this will simplify making sure that `null` is explicitly sent instead
    of `undefined`.

---
+++ hooks/useMaybeFragment.ts
@@ -0,0 +1,14 @@
+import { useFragment } from "react-relay";
+import type { GraphQLTaggedNode } from "react-relay";
+
+type Fragment = Readonly<{
+  " $data"?: unknown;
+  " $fragmentRefs": unknown;
+}>;
+
+export default function useMaybeFragment<TKey extends Fragment>(
+  fragmentInput: GraphQLTaggedNode,
+  fragmentRef?: TKey
+) {
+  return useFragment<TKey>(fragmentInput, fragmentRef ?? null);
+}
