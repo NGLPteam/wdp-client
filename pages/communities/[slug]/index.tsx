4b6288f5dd313deb5fb75f379cfbf07ccec758ac
Zach Davis <zach@castironcoding.com>
2021-09-05 06:22:14 -0700

    [C] Refactor detail index route redirects
    When the active route has a `redirect` property, the application should
    redirect the user to that URL. This commit refactors existing pages so
    that this redirect can happen in one page, in `_app`, rather than in
    every individual page.

--- pages/communities/[slug]/index.tsx
+++ pages/communities/[slug]/index.tsx
@@ -1,24 +1,9 @@
 import React from "react";
-import CommunityLayout from "components/composed/community/CommunityLayout";
-import { useRouter } from "next/router";
-import { RouteHelper } from "routes";
 import { Page } from "types/page";
 
+// This is a redirect route. See routes/baseRoutes.ts.
 const CommunityDetail: Page = () => {
-  const router = useRouter();
-  const activeRoute = RouteHelper.activeRoute();
-
-  // Nothing to see here, move on to collections
-  router.replace({
-    pathname: activeRoute?.redirect,
-    query: { ...router.query },
-  });
-
   return null;
-};
-
-CommunityDetail.getLayout = (page) => {
-  return <CommunityLayout>{page}</CommunityLayout>;
 };
 
 export default CommunityDetail;
