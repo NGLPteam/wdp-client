e6caa539b29fd37dd2fbe317e830f5003f40b0d6
Alexa Grey <alexa@castironcoding.com>
2021-09-10 18:18:20 -0700

    [C] Add type-safety around setting boolean values

--- types/form-fields.ts
+++ types/form-fields.ts
@@ -1,4 +1,5 @@
-import { CSSProperties, ChangeEvent } from "react";
+import type { CSSProperties, ChangeEvent } from "react";
+import type { FieldValues, Path, UseFormSetValue } from "react-hook-form";
 
 export interface CheckboxProps {
   checked?: boolean;
@@ -7,3 +8,27 @@ export interface CheckboxProps {
   style?: CSSProperties;
   onChange?: (e: ChangeEvent<Element>) => void;
 }
+
+export type BooleanFieldPaths<T extends FieldValues> = {
+  [K in keyof T]-?: NonNullable<T[K]> extends boolean ? K : never;
+}[keyof T];
+
+export type BooleanFields<T extends FieldValues> = Pick<
+  T,
+  BooleanFieldPaths<T>
+>;
+
+export type BooleanPath<T extends FieldValues> = Path<T> &
+  Path<BooleanFields<T>>;
+
+/**
+ * This generic overloads the RHF setValue to allow setting a boolean directly.
+ */
+export type SetBooleanValue<T extends FieldValues> = (<
+  TFieldName extends BooleanPath<T>
+>(
+  name: TFieldName,
+  value: boolean,
+  options?: Parameters<UseFormSetValue<T>>[2]
+) => ReturnType<UseFormSetValue<T>>) &
+  UseFormSetValue<T>;
