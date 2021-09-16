21d1ea90b8c92a84b828151e053f9d9f504efbf6
jen-castiron <jen@castironcoding.com>
2021-09-16 12:55:13 -0700

    [C] Pass route object to TabNav

--- components/atomic/TabNav/TabNav.tsx
+++ components/atomic/TabNav/TabNav.tsx
@@ -1,23 +1,41 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { RouteHelper } from "routes";
+import { NamedLink } from "components/atomic";
 import * as Styled from "./TabNav.styles";
 
-const TabNav = ({ children }: Props) => {
-  return (
+type NamedLinkProps = React.ComponentProps<typeof NamedLink>;
+
+const TabNav = ({ links }: Props) => {
+  const { t } = useTranslation();
+
+  // Check if the route should be active
+  const activeRoute = RouteHelper.activeRoute();
+  function isActive(routeName: string): boolean | undefined {
+    return activeRoute?.name.includes(routeName);
+  }
+
+  return links ? (
     <Styled.Nav>
       <Styled.List className="l-flex">
-        {children &&
-          children.map((child, i) => (
-            <Styled.Item key={i}>{child}</Styled.Item>
-          ))}
+        {links.map(({ label, ...namedLinkProps }, i) => (
+          <Styled.Item key={i}>
+            <NamedLink {...namedLinkProps} passHref>
+              <Styled.Tab active={isActive(namedLinkProps.route)}>
+                {t(label || "")}
+              </Styled.Tab>
+            </NamedLink>
+          </Styled.Item>
+        ))}
       </Styled.List>
     </Styled.Nav>
-  );
+  ) : null;
 };
-interface Props {
-  children: React.ReactNode[];
+interface Link extends NamedLinkProps {
+  label?: string;
 }
-
-TabNav.Tab = Styled.Tab;
-TabNav.Tab.displayName = "TabNav.Tab";
+interface Props {
+  links?: Link[];
+}
 
 export default TabNav;
