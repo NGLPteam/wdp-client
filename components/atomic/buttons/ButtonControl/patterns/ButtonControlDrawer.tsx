f2278f609678298b8016a9d082150366047ea2e1
jen-castiron <jen@castironcoding.com>
2021-09-20 13:34:11 -0700

    [B] Fix drawer type issue in ButtonControlGroup

--- components/atomic/buttons/ButtonControl/patterns/ButtonControlDrawer.tsx
+++ components/atomic/buttons/ButtonControl/patterns/ButtonControlDrawer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { DrawerLink, ButtonControl } from "components/atomic";
 type BaseProps = React.ComponentProps<typeof ButtonControl>;
 type DrawerLinkProps = React.ComponentProps<typeof DrawerLink>;
@@ -6,16 +6,20 @@ type DrawerLinkProps = React.ComponentProps<typeof DrawerLink>;
 /**
  * A button control that opens a drawer
  */
-const ButtonControlDrawer = ({ drawer, icon, children }: Props) => {
-  return (
-    <DrawerLink drawer={drawer} passHref>
-      <ButtonControl as="a" icon={icon}>
-        {children}
-      </ButtonControl>
-    </DrawerLink>
-  );
-};
+const ButtonControlDrawer = forwardRef(
+  ({ drawer, icon, children }: Props, ref) => {
+    return (
+      <DrawerLink drawer={drawer} passHref>
+        <ButtonControl as="a" icon={icon} ref={ref}>
+          {children}
+        </ButtonControl>
+      </DrawerLink>
+    );
+  }
+);
 
-type Props = BaseProps & Pick<DrawerLinkProps, "drawer">;
+interface Props extends BaseProps {
+  drawer: DrawerLinkProps["drawer"];
+}
 
 export default ButtonControlDrawer;
