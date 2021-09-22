51c5b1c03206f884cd763ae3aeaabff38ab70e86
jen-castiron <jen@castironcoding.com>
2021-09-22 16:15:06 -0700

    [F] Create generic columns with default sort set to false

---
+++ components/composed/model/ModelColumns/StringColumn.tsx
@@ -0,0 +1,23 @@
+import { Column } from "react-table";
+import { PartialColumnish, Node } from "./types";
+
+type Props<T extends Node> = PartialColumnish<T> & {
+  Header: string;
+  id: string;
+};
+
+const StringColumn = <T extends Node>({
+  Header,
+  id,
+  ...props
+}: Props<T>): Column<T> => {
+  return {
+    Header,
+    id,
+    accessor: (originalRow: T) => originalRow[id],
+    disableSortBy: true,
+    ...props,
+  };
+};
+
+export default StringColumn;
