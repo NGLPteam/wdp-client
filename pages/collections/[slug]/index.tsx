2e3cb3ecbf1e078588e69bfd7d241bdbe7a93030
jen-castiron <jen@castironcoding.com>
2021-10-28 10:23:38 -0700

    [B] Fix collection slug redirect caching issue

--- pages/collections/[slug]/index.tsx
+++ pages/collections/[slug]/index.tsx
@@ -1,42 +1,20 @@
+import React from "react";
 import { graphql } from "react-relay";
-import { useRouteSlug } from "hooks";
 import { SlugCollectionsPageQuery as Query } from "@/relay/SlugCollectionsPageQuery.graphql";
 import { GetLayout } from "types/page";
-import { useRouter } from "next/router";
 import CollectionLayoutQuery from "components/composed/collection/CollectionLayoutQuery";
-import { RouteHelper } from "routes";
-import { LoadingCircle } from "components/atomic";
+import CollectionSlugRedirect from "components/composed/collection/CollectionSlugRedirect";
 
-const getLayout: GetLayout<Props> = (props) => {
-  return <CollectionLayoutQuery<Query, Props> query={query} {...props} />;
-};
 function CollectionSlug({ data }: Props) {
-  const slug = useRouteSlug();
-  const router = useRouter();
-  const collectionRoute = RouteHelper.findRouteByName(
-    "collection.child.collections"
-  );
-  const itemRoute = RouteHelper.findRouteByName("collection.child.items");
-
-  const total = data?.collection?.collections?.pageInfo?.totalCount;
-
-  if (total && total > 0 && collectionRoute) {
-    router.replace({
-      pathname: collectionRoute.path,
-      query: { slug },
-    });
-  } else if (total === 0 && itemRoute) {
-    router.replace({
-      pathname: itemRoute.path,
-      query: { slug },
-    });
-  }
-
-  return <LoadingCircle />;
+  return <CollectionSlugRedirect data={data?.collection} />;
 }
 
 type Props = {
   data: Query["response"];
+};
+
+const getLayout: GetLayout<Props> = (props) => {
+  return <CollectionLayoutQuery<Query, Props> query={query} {...props} />;
 };
 
 CollectionSlug.getLayout = getLayout;
@@ -47,11 +25,7 @@ const query = graphql`
   query SlugCollectionsPageQuery($collectionSlug: Slug!) {
     collection(slug: $collectionSlug) {
       ...CollectionLayoutQueryFragment
-      collections {
-        pageInfo {
-          totalCount
-        }
-      }
+      ...CollectionSlugRedirectFragment
     }
   }
 `;
