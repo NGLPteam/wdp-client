da27804eca2a9ae672f719970516ef43ecd7a1d9
jen-castiron <jen@castironcoding.com>
2021-11-17 11:57:23 -0800

    [F] Build and style instance header

---
+++ packages/frontend/components/global/AppHeader/AppHeader.styles.ts
@@ -0,0 +1,6 @@
+import { pxToRem } from "@wdp/lib/theme/functions";
+import styled from "styled-components";
+
+export const Header = styled.header`
+  padding-block: ${pxToRem(14)};
+`;
--- packages/frontend/components/global/AppHeader/AppHeader.tsx
+++ packages/frontend/components/global/AppHeader/AppHeader.tsx
@@ -3,11 +3,18 @@ import { graphql } from "react-relay";
 import { useMaybeFragment } from "@wdp/lib/api/hooks";
 import InstallationName from "../InstallationName";
 import { AppHeaderFragment$key } from "@/relay/AppHeaderFragment.graphql";
+import * as Styled from "./AppHeader.styles";
 
 function AppHeader({ data }: Props) {
   const appData = useMaybeFragment(fragment, data);
 
-  return <header>{appData && <InstallationName data={appData} />}</header>;
+  return (
+    <Styled.Header className="a-bg-custom20">
+      <div className="l-container-wide">
+        {appData && <InstallationName data={appData} />}
+      </div>
+    </Styled.Header>
+  );
 }
 
 interface Props {
