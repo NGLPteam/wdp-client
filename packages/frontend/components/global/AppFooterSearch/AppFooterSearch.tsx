3f07a1e0de5202cc741bad056d1410f4276c97cc
jen-castiron <jen@castironcoding.com>
2021-11-18 15:41:42 -0800

    [F] Style footer select input form

--- packages/frontend/components/global/AppFooterSearch/AppFooterSearch.tsx
+++ packages/frontend/components/global/AppFooterSearch/AppFooterSearch.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useUID } from "react-uid";
 import { IconFactory } from "../../factories";
+import * as Styled from "./AppFooterSearch.styles";
 
-export default function AppFooterSearch({ placeholder = "search.label" }) {
+export default function AppFooterSearch() {
   const { t } = useTranslation();
+  const searchId = useUID();
 
   return (
-    <div className="a-button-secondary-sm l-flex l-flex--gap l-flex--align-center">
-      <IconFactory icon="search" />
-      <span className="t-copy-lighter">{t(placeholder)}</span>
-    </div>
+    <Styled.SearchForm className="a-button-secondary-sm l-flex l-flex--gap">
+      <Styled.SearchLabel htmlFor={searchId}>
+        <span className="a-hidden">{t("search.label")}</span>
+        <IconFactory icon="search" role="presentation" />
+      </Styled.SearchLabel>
+      <Styled.SearchInput
+        id={searchId}
+        type="search"
+        placeholder={t("search.placeholder")}
+      />
+      <button className="a-hidden">{t("search.submit")}</button>
+    </Styled.SearchForm>
   );
 }
