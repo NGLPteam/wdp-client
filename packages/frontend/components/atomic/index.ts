f78ad14d7113d05d0189022b2a4a0b858fb4c83f
jen-castiron <jen@castironcoding.com>
2021-11-17 18:11:01 -0800

    [F] Add search button

--- packages/frontend/components/atomic/index.ts
+++ packages/frontend/components/atomic/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Dropdown } from "./Dropdown";
 export { default as Avatar } from "./Avatar";
+export { default as SearchButton } from "./SearchButton";
