28731540ffef725dcdf4e67f0c93094fb6b764df
jen-castiron <jen@castironcoding.com>
2021-12-02 10:51:01 -0800

    [C] Add journal and volume data to issue hero

--- packages/frontend/__generated__/IssueHeroFragment.graphql.ts
+++ packages/frontend/__generated__/IssueHeroFragment.graphql.ts
@@ -12,6 +12,12 @@ export type IssueHeroFragment = {
     readonly thumbnail: {
         readonly " $fragmentRefs": FragmentRefs<"CoverImageFragment">;
     } | null;
+    readonly journal: {
+        readonly title?: string | undefined;
+    } | null;
+    readonly volume: {
+        readonly title?: string | undefined;
+    } | null;
     readonly " $refType": "IssueHeroFragment";
 };
 export type IssueHeroFragment$data = IssueHeroFragment;
@@ -22,19 +28,31 @@ export type IssueHeroFragment$key = {
 
 
 
-const node: ReaderFragment = {
+const node: ReaderFragment = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "title",
+  "storageKey": null
+},
+v1 = [
+  {
+    "kind": "InlineFragment",
+    "selections": [
+      (v0/*: any*/)
+    ],
+    "type": "Collection",
+    "abstractKey": null
+  }
+];
+return {
   "argumentDefinitions": [],
   "kind": "Fragment",
   "metadata": null,
   "name": "IssueHeroFragment",
   "selections": [
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "title",
-      "storageKey": null
-    },
+    (v0/*: any*/),
     {
       "alias": null,
       "args": null,
@@ -64,10 +82,43 @@ const node: ReaderFragment = {
         }
       ],
       "storageKey": null
+    },
+    {
+      "alias": "journal",
+      "args": [
+        {
+          "kind": "Literal",
+          "name": "schema",
+          "value": "nglp:journal"
+        }
+      ],
+      "concreteType": null,
+      "kind": "LinkedField",
+      "name": "ancestorOfType",
+      "plural": false,
+      "selections": (v1/*: any*/),
+      "storageKey": "ancestorOfType(schema:\"nglp:journal\")"
+    },
+    {
+      "alias": "volume",
+      "args": [
+        {
+          "kind": "Literal",
+          "name": "schema",
+          "value": "nglp:journal_volume"
+        }
+      ],
+      "concreteType": null,
+      "kind": "LinkedField",
+      "name": "ancestorOfType",
+      "plural": false,
+      "selections": (v1/*: any*/),
+      "storageKey": "ancestorOfType(schema:\"nglp:journal_volume\")"
     }
   ],
   "type": "Collection",
   "abstractKey": null
 };
-(node as any).hash = '5618fd0b848cf910368957e9a30b1016';
+})();
+(node as any).hash = '4e777a591f6b255d108fa217fe2885a7';
 export default node;
