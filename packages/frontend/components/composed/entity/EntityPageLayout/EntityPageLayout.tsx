bf4ff80818496e79dbdccf8e0457ed0b636c64b2
jen-castiron <jen@castironcoding.com>
2021-12-07 14:34:56 -0800

    [F] Add image to EntityPageContent

--- packages/frontend/components/composed/entity/EntityPageLayout/EntityPageLayout.tsx
+++ packages/frontend/components/composed/entity/EntityPageLayout/EntityPageLayout.tsx
@@ -3,12 +3,14 @@ import { useMaybeFragment } from "@wdp/lib/api/hooks";
 import { graphql } from "react-relay";
 import ReactMarkdown from "react-markdown";
 import { EntityPageLayoutFragment$key } from "@/relay/EntityPageLayoutFragment.graphql";
+import { ContentImage } from "components/atomic";
 
 export default function EntityPageLayout({ data }: Props) {
   const page = useMaybeFragment(fragment, data);
 
   return page ? (
     <div className="t-rte">
+      {page.heroImage && <ContentImage data={page.heroImage} />}
       <h3>{page.title}</h3>
       <ReactMarkdown>{page.body}</ReactMarkdown>
     </div>
@@ -26,5 +28,8 @@ const fragment = graphql`
   fragment EntityPageLayoutFragment on Page {
     title
     body
+    heroImage {
+      ...ContentImageFragment
+    }
   }
 `;
