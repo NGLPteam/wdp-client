6a96ed597fe6aa867ec3fa8afb36e68df2463448
jen-castiron <jen@castironcoding.com>
2021-12-08 15:57:56 -0800

    [C] Adjust article layout so pages can have block content

--- packages/frontend/components/composed/contribution/ContributionsBlock/CollectionContributionsBlock.tsx
+++ packages/frontend/components/composed/contribution/ContributionsBlock/CollectionContributionsBlock.tsx
@@ -8,10 +8,7 @@ import {
   CollectionContributionsBlockFragment$data,
 } from "@/relay/CollectionContributionsBlockFragment.graphql";
 
-const CollectionContributionsBlock = ({
-  data,
-  background = "custom10",
-}: Props) => {
+const CollectionContributionsBlock = ({ data, background }: Props) => {
   const contributors = useMaybeFragment(fragment, data);
 
   const showAvatars = useMemo(() => {
