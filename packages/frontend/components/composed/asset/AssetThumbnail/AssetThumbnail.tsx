7f723ba0c1c166811ad4a560e12f407a336cd23e
jen-castiron <jen@castironcoding.com>
2021-12-10 13:49:12 -0800

    [F] Add asset thumbnail hover and focus styles

--- packages/frontend/components/composed/asset/AssetThumbnail/AssetThumbnail.tsx
+++ packages/frontend/components/composed/asset/AssetThumbnail/AssetThumbnail.tsx
@@ -3,13 +3,16 @@ import { useMaybeFragment } from "@wdp/lib/api/hooks";
 import { graphql } from "react-relay";
 import { FileThumbnail } from "components/atomic";
 import { AssetThumbnailFragment$key } from "@/relay/AssetThumbnailFragment.graphql";
+import * as Styled from "./AssetThumbnail.styles";
 
 export default function AssetThumbnail({ data }: Props) {
   const asset = useMaybeFragment(fragment, data);
   const image = asset?.preview?.storage ? asset.preview.image?.webp : null;
 
   return asset ? (
-    <FileThumbnail alt={image?.alt} url={image?.url} kind={asset.kind} />
+    <Styled.ImageLink href="#">
+      <FileThumbnail alt={image?.alt} url={image?.url} kind={asset.kind} />
+    </Styled.ImageLink>
   ) : null;
 }
 
@@ -20,6 +23,7 @@ interface Props {
 const fragment = graphql`
   fragment AssetThumbnailFragment on Asset {
     kind
+    slug
     preview {
       storage
       image: small {
