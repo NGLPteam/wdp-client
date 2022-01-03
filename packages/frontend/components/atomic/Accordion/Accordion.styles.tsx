db518da411ee7b7e8f9c66ab6c8902f643e4f23d
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-03 13:43:30 -0800

    [F] Build mobile Accordion component

---
+++ packages/frontend/components/atomic/Accordion/Accordion.styles.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { pxToRem } from "@wdp/lib/theme/functions";
+import { tLabel, tCopySmall } from "theme/mixins";
+import { IconFactory } from "components/factories";
+
+export const Accordion = styled.details``;
+
+export const Label = styled.summary`
+  ${tLabel("lg")}
+  font-weight: 400;
+  list-style: none;
+
+  display: flex;
+  align-items: center;
+  gap: ${pxToRem(10)};
+`;
+
+export const Arrow = styled(IconFactory)`
+  margin-block-start: ${pxToRem(3)};
+`;
+
+export const List = styled.ul`
+  padding-inline-start: ${pxToRem(20)};
+  padding-block-start: var(--padding-rg);
+`;
+
+export const Item = styled.li`
+  white-space: nowrap;
+  ${tCopySmall}
+
+  & + & {
+    margin-block-start: var(--padding-rg);
+  }
+`;
---
+++ packages/frontend/components/atomic/Accordion/Accordion.tsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from "react";
+import * as Styled from "./Accordion.styles";
+import { IconKeys } from "components/factories/IconFactory/IconFactory";
+
+export default function Accordion({ label, menuItems }: Props) {
+  const ref = useRef<HTMLDetailsElement>(null);
+  const [icon, toggleIcon] = useState<IconKeys>("chevronDown");
+
+  const onToggle = () => {
+    if (!ref.current?.open) {
+      toggleIcon("chevronUp");
+      return;
+    }
+    toggleIcon("chevronDown");
+  };
+
+  const submenu =
+    menuItems &&
+    menuItems.map((item, i) => <Styled.Item key={i}>{item}</Styled.Item>);
+
+  return (
+    <Styled.Accordion ref={ref}>
+      <Styled.Label onClick={onToggle}>
+        {label}
+        <Styled.Arrow icon={icon} role="presentation" />
+      </Styled.Label>
+      <Styled.List>{submenu}</Styled.List>
+    </Styled.Accordion>
+  );
+}
+
+type Props = {
+  label: string;
+  menuItems?: JSX.Element[];
+};
