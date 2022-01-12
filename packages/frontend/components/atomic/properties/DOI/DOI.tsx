7da386e49d85ed18c3d1219dfff8fbf73eeb0e1e
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-12 13:11:53 -0800

    [B] Remove label from DOIs (#351)

--- packages/frontend/components/atomic/properties/DOI/DOI.tsx
+++ packages/frontend/components/atomic/properties/DOI/DOI.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { graphql } from "react-relay";
-import { useTranslation } from "react-i18next";
 import { useMaybeFragment } from "@wdp/lib/api/hooks";
 import { DOIFragment$key } from "@/relay/DOIFragment.graphql";
 
 export default function DOI({ data }: Props) {
   const entity = useMaybeFragment(fragment, data);
 
-  const { t } = useTranslation();
-
   return entity?.doi ? (
     <div>
-      {t("metadata.doi")}{" "}
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href={entity.doi}
-        className="t-copy-light a-link"
-      >
+      <a target="_blank" rel="noreferrer" href={entity.doi} className="a-link">
         {entity.doi}
       </a>
     </div>
