70420dd3534a9c77b7402d6f468e21089a0a5ad4
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-19 16:35:34 -0800

    [C] Show groups with default legend

--- packages/admin/components/api/SchemaInstanceForm/Factories/GroupProperty/GroupProperty.tsx
+++ packages/admin/components/api/SchemaInstanceForm/Factories/GroupProperty/GroupProperty.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { graphql } from "relay-runtime";
 import { useFragment } from "relay-hooks";
+import { useTranslation } from "react-i18next";
 import SchemaProperty from "../SchemaProperty";
 import { FieldsetSection, FormGrid } from "components/forms";
 import type { GroupPropertyFragment$key } from "@/relay/GroupPropertyFragment.graphql";
 
 export default function GroupProperty(props: Props) {
   const group = useFragment<GroupPropertyFragment$key>(fragment, props.group);
+  const { t } = useTranslation();
 
   // Schema property inputs should be laid out in a grid,
   // same as the global properties
@@ -18,7 +20,15 @@ export default function GroupProperty(props: Props) {
         ))}
       </FormGrid>
     </FieldsetSection>
-  ) : null;
+  ) : (
+    <FieldsetSection label={t("forms.fields.fields")}>
+      <FormGrid>
+        {group.properties.map((prop, index) => (
+          <SchemaProperty field={prop} key={index} />
+        ))}
+      </FormGrid>
+    </FieldsetSection>
+  );
 }
 
 interface Props {
