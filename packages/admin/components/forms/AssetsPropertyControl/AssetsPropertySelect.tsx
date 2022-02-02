cb16a905aa3442ba2248563d0a4ef71fb6e90de0
jen-castiron <jen@castironcoding.com>
2022-02-02 12:37:41 -0800

    [F] Update AssetsPropertyControl with multiselect, drag drop list

---
+++ packages/admin/components/forms/AssetsPropertyControl/AssetsPropertySelect.tsx
@@ -0,0 +1,89 @@
+import React, { forwardRef, Ref, useCallback, useRef } from "react";
+import AssetPropertySelect from "../AssetPropertySelect";
+import AssetsPropertySelectedList from "./AssetsPropertySelectedList";
+
+type BaseProps = React.ComponentProps<typeof AssetPropertySelect>;
+
+const AssetsPropertySelect = forwardRef(
+  (
+    { label, required, options, isWide, onChange, value = [], ...props }: Props,
+    ref: Ref<HTMLSelectElement>
+  ) => {
+    const selectRef = useRef<HTMLSelectElement | null>(null);
+
+    const handleOnChange = useCallback(
+      (e) => {
+        if (!e || !e.target.value) return;
+
+        const selectedValue = e.target.value;
+
+        if (value && !value.includes(selectedValue)) {
+          onChange([...value, selectedValue]);
+        } else if (!value) {
+          onChange([selectedValue]);
+        }
+
+        if (selectRef.current) selectRef.current.value = "";
+      },
+      [onChange, selectRef, value]
+    );
+
+    const handleRemove = useCallback(
+      (index: number) => {
+        const newValue = [...value];
+        newValue.splice(index, 1);
+        onChange(newValue);
+      },
+      [onChange, value]
+    );
+
+    const handleDragEnd = useCallback(
+      (sourceIndex: number, destinationIndex: number) => {
+        const newValue = [...value];
+        const sourceItem = newValue.splice(sourceIndex, 1)[0];
+
+        newValue.splice(destinationIndex, 0, sourceItem);
+        onChange(newValue);
+      },
+      [onChange, value]
+    );
+
+    return (
+      <div>
+        <AssetPropertySelect
+          label={label}
+          required={required}
+          options={options}
+          isWide={isWide}
+          onChange={handleOnChange}
+          ref={(e) => {
+            if (typeof ref === "function") ref(e);
+            selectRef.current = e;
+          }}
+          {...props}
+        />
+        <AssetsPropertySelectedList
+          options={options}
+          value={value}
+          onRemove={handleRemove}
+          onDragEnd={handleDragEnd}
+        />
+      </div>
+    );
+  }
+);
+
+export type Option = {
+  kind: string;
+  label: string;
+  value: string;
+};
+
+export type Value = string[];
+
+type Props = BaseProps & {
+  value: Value;
+  options: Option[];
+};
+
+export default AssetsPropertySelect;
---
+++ packages/admin/components/forms/AssetsPropertyControl/AssetsPropertySelectedList.tsx
@@ -0,0 +1,75 @@
+import React, { useCallback, useMemo } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import BaseArrayList, { BaseArrayListItem } from "../BaseArrayList";
+import { Option, Value } from "./AssetsPropertySelect";
+
+export default function AssetsPropertySelectedList({
+  options,
+  value,
+  onRemove,
+  onDragEnd,
+}: Props) {
+  const selectedValues = useMemo(() => {
+    return value.map((v) => {
+      const item = options.find((o) => o.value === v);
+      return { label: item?.label || v, value: v };
+    });
+  }, [value, options]);
+
+  const handleRemove = useCallback(
+    (index) => {
+      if (onRemove) onRemove(index);
+    },
+    [onRemove]
+  );
+
+  const handleDragEnd = useCallback(
+    ({ destination, source }) => {
+      if (!destination || !source || destination.index === source.index) return;
+
+      onDragEnd(source.index, destination.index);
+    },
+    [onDragEnd]
+  );
+
+  return value ? (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="assetsPropertyDroppable">
+        {(provided, snapshot) => (
+          <BaseArrayList
+            column
+            ref={provided.innerRef}
+            isDraggingOver={snapshot.isDraggingOver}
+            {...provided.droppableProps}
+          >
+            {selectedValues.map((v, i) => (
+              <Draggable key={v.value} draggableId={v.value} index={i}>
+                {(provided, snapshot) => (
+                  <BaseArrayListItem
+                    key={v.value}
+                    onRemove={() => handleRemove(i)}
+                    ref={provided.innerRef}
+                    isDragging={snapshot.isDragging}
+                    draggableStyles={provided.draggableProps.style}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {v.label}
+                  </BaseArrayListItem>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </BaseArrayList>
+        )}
+      </Droppable>
+    </DragDropContext>
+  ) : null;
+}
+
+interface Props {
+  options: Option[];
+  value: Value;
+  onRemove: (index: number) => void;
+  onDragEnd: (sourceIndex: number, destinationIndex: number) => void;
+}
