d9b0ade8c981704c78c949f20fd42199ffcb536e
jen-castiron <jen@castironcoding.com>
2022-02-08 11:45:51 -0800

    [B] Adjust underline for style2 and 3 - WDP-579

--- packages/frontend/theme/mixins/appearance.ts
+++ packages/frontend/theme/mixins/appearance.ts
@@ -146,24 +146,43 @@ export function aFocus(content?: CssContent) {
   `;
 }
 
-export const aLinkHoverFocus = `
+// The box shadow's line height should be the same - .07em,
+// but it's placement changes based on the font style
+function getBoxShadow(fontStyle: string): string {
+  switch (fontStyle) {
+    case "style3":
+      return "inset 0 -0.11em 0 0 var(--background-color), inset 0 -0.18em 0 0 var(--color-light)";
+
+    case "style2":
+      return "inset 0 -0.07em 0 0 var(--background-color), inset 0 -0.14em 0 0 var(--color-light)";
+
+    default:
+      return "0 0.01em 0 0 var(--color-light), inset 0 -0.06em 0 0 var(--color-light)";
+  }
+}
+
+export const aLinkHoverFocus = css`
   color: var(--color-base);
-  text-shadow: 0.04em 0.1em var(--background-color),
-    -0.04em 0.1em var(--background-color);
-  box-shadow: 0 0.07em 0 0 var(--color-light);
+  text-shadow: 0.04em 0.04em var(--background-color),
+    -0.04em 0.04em var(--background-color),
+    -0.04em -0.04em var(--background-color),
+    0.04em -0.04em var(--background-color), -0.04em 0 var(--background-color),
+    0.04em 0 var(--background-color);
+  box-shadow: ${({ theme }) => getBoxShadow(theme.fontStyle)};
 `;
 
 export const aLinkBase = `
   cursor: pointer;
   transition: ${transition.color}, box-shadow 0.17s ease-in,
     text-shadow 0.17s ease-in;
+    
 
   &[aria-current] {
     font-weight: var(--font-weight-medium);
   }
 `;
 
-export const aLink = `
+export const aLink = css`
   ${aLinkBase}
 
   &:hover {
@@ -180,7 +199,7 @@ export const aLink = `
 `;
 
 // Use this mixin to style text that is inside a link
-export const aLinkText = `
+export const aLinkText = css`
   display: inline;
   ${aLinkBase}
 
