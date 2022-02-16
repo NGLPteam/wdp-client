13f068cded8e459f32bd71fdac1160d50fccf10c
jen-castiron <jen@castironcoding.com>
2022-02-16 14:18:30 -0800

    [F] Adjust dashboard styling for mobile

--- packages/admin/components/composed/dashboard/DashboardLayout/DashboardLayout.styles.ts
+++ packages/admin/components/composed/dashboard/DashboardLayout/DashboardLayout.styles.ts
@@ -1,12 +1,15 @@
 import { pxToRem } from "@wdp/lib/theme/functions";
 import styled from "styled-components";
+import { fluidScale, respond } from "theme/mixins/base";
 
 export const Wrapper = styled.div`
   display: grid;
-  gap: ${pxToRem(52)};
-  grid-template-columns: repeat(auto-fill, minmax(474px, 1fr));
+  gap: ${fluidScale("52px", "24px")};
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
   grid-template-rows: repeat(2, auto);
   padding-block-start: ${pxToRem(80)};
+
+  ${respond(`grid-template-columns: 1fr;`, 40)}
 
   > *:first-child {
     grid-row: -1 / 1;
