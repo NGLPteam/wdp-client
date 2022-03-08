e57c6d86dbdd3eb291c7abf347e462f602301ffd
jen-castiron <jen@castironcoding.com>
2022-03-08 09:47:31 -0800

    [C] Upgrade storybook

--- packages/frontend/babel.config.js
+++ packages/frontend/babel.config.js
@@ -8,5 +8,8 @@ module.exports = {
         ssr: true,
       },
     ],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
   ],
 };
