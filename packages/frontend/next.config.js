3ddba67ef2fa4b94b0659d1c2d175040f63fca48
Zach Davis <zach@castironcoding.com>
2022-04-01 16:30:04 -0700

    [F] Allow image domains to be set in via env

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,15 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const transpiler = require("next-transpile-modules");
 
-const imageDomains = [
-  "localhost:6007",
-  "localhost:3001",
-  "localhost:10042",
-  "localhost",
-  "api.staging.nglp.org",
-  "wdp-api-staging.s3.us-west-2.amazonaws.com",
-  "wdp-api-stg.s3.us-west-2.amazonaws.com",
-];
+const imageDomains = process.env.IMAGE_DOMAINS
+  ? process.env.IMAGE_DOMAINS.split(",")
+  : [
+      "localhost:6007",
+      "localhost:3001",
+      "localhost:10042",
+      "localhost",
+      "api.staging.nglp.org",
+      "wdp-api-staging.s3.us-west-2.amazonaws.com",
+      "wdp-api-stg.s3.us-west-2.amazonaws.com",
+    ];
 
 const withTM = transpiler(["@spissvinkel/alea"]);
 
