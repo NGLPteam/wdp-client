e4be9ae5bc6ff3d387b2fa4878a05aa8d7b2dd56
jen-castiron <jen@castironcoding.com>
2022-04-25 11:15:44 -0700

    [B] Fix styling of search filter boolean

--- packages/admin/components/composed/search/SearchFilter/Filters/SearchFilterBoolean.tsx
+++ packages/admin/components/composed/search/SearchFilter/Filters/SearchFilterBoolean.tsx
@@ -2,20 +2,22 @@ import { graphql, useFragment } from "react-relay";
 import { useFormContext } from "react-hook-form";
 import { Checkbox } from "components/forms";
 import { SearchFilterBooleanFragment$key } from "@/relay/SearchFilterBooleanFragment.graphql";
+import BaseInputWrapper from "components/forms/BaseInputWrapper";
 
 export default function SearchFilterBoolean({ data }: Props) {
   const filter = useFragment(fragment, data);
 
   const { register } = useFormContext();
 
+  const name = `${filter.searchPath.replace(".", "-")}--equals`;
+
   // Checkbox are never search "in" - just "equals"
   return (
-    <Checkbox
-      value="true"
-      {...register(`${filter.searchPath.replace(".", "-")}--equals`)}
-    >
-      {filter.label}
-    </Checkbox>
+    <BaseInputWrapper name={name} label={filter.label} hideLabel>
+      <Checkbox value="true" {...register(name)}>
+        {filter.label}
+      </Checkbox>
+    </BaseInputWrapper>
   );
 }
 
