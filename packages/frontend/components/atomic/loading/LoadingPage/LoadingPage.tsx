463746546858986549d2b650a8fff5e4488db2c7
jen-castiron <jen@castironcoding.com>
2022-05-31 10:25:27 -0700

    [B] Add aria attributes to frontend loading indicator

--- packages/frontend/components/atomic/loading/LoadingPage/LoadingPage.tsx
+++ packages/frontend/components/atomic/loading/LoadingPage/LoadingPage.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import LoadingSpinner from "../LoadingSpinner";
 import * as Styled from "./LoadingPage.styles";
 
-export default function LoadingPage() {
+interface Props {
+  label?: string;
+}
+
+export default function LoadingPage({ label }: Props) {
+  const { t } = useTranslation();
+
   return (
-    <Styled.Wrapper>
-      <LoadingSpinner />
+    <Styled.Wrapper
+      aria-live="polite"
+      aria-busy="true"
+      aria-valuemin={0}
+      aria-valuemax={100}
+      role="progressbar"
+      aria-valuetext={t(label || "common.loading")}
+    >
+      <LoadingSpinner label={label} />
     </Styled.Wrapper>
   );
 }
