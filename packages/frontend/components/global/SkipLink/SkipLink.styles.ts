134a0684339886467a64f782f5fd7459c22fae5f
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-06-02 09:52:56 -0700

    [F] Add skip to content link (#496)
    Resolves wdp-732

---
+++ packages/frontend/components/global/SkipLink/SkipLink.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const SkipLink = styled.a`
+  position: fixed;
+  top: 10px;
+  left: -400px;
+  transition: left 0.25s ease;
+
+  &:focus {
+    left: 10px;
+    outline: none;
+  }
+`;
+
+export const SRText = styled.span`
+  display: none;
+`;
---
+++ packages/frontend/components/global/SkipLink/SkipLink.tsx
@@ -0,0 +1,23 @@
+import { useTranslation } from "react-i18next";
+import * as Styled from "./SkipLink.styles";
+import { Alert } from "components/atomic";
+
+interface Props {
+  toId: string;
+  label?: string;
+}
+
+export default function SkipLink({ toId, label }: Props) {
+  const { t } = useTranslation();
+  const href = `#${toId}`;
+
+  return (
+    <Styled.SkipLink href={href}>
+      <Alert
+        message={label ?? t("nav.skip_to_content_label")}
+        color="blue"
+        badge
+      />
+    </Styled.SkipLink>
+  );
+}
