cf02a120bc34d1082976678b172274fab792cca6
Jen Wardwell <jen@castironcoding.com>
2022-06-03 12:54:48 -0700

    [F] Add sort by created date (#491)
    - Resolves WDP-737

--- packages/frontend/components/composed/search/SearchOrderBy/SearchOrderBy.tsx
+++ packages/frontend/components/composed/search/SearchOrderBy/SearchOrderBy.tsx
@@ -36,6 +36,8 @@ export default function SearchOrderBy() {
         <option disabled>{t("list.order_by_label")}</option>
         <option value="PUBLISHED_ASCENDING">Publish Date, Ascending</option>
         <option value="PUBLISHED_DESCENDING">Publish Date, Descending</option>
+        <option value="OLDEST">Date Created, Ascending</option>
+        <option value="RECENT">Date Created, Descending</option>
         <option value="TITLE_ASCENDING">Title, A-Z</option>
         <option value="TITLE_DESCENDING">Title, Z-A</option>
         <option value="SCHEMA_NAME_ASCENDING">Schema, Ascending</option>
