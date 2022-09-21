c35ee3f8a47a5d8917c033171b24f8fb6f37fff7
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-20 17:58:24 -0700

    [C] Clean up settings reducer

--- packages/frontend/components/composed/analytics/ArticleAnalyticsBlock/settingsReducer.ts
+++ packages/frontend/components/composed/analytics/ArticleAnalyticsBlock/settingsReducer.ts
@@ -21,57 +21,57 @@ export type Action = {
   value: string;
 };
 
+const getDateVars = (minDate: string | null, value: string) => {
+  const now = Date.now();
+
+  const hasMultipleYears = minDate
+    ? differenceInCalendarYears(parseISO(minDate), now) > 0
+    : true;
+
+  let startDate;
+  let precision;
+  switch (value) {
+    case "week":
+      startDate = subDays(now, 7);
+      precision = "DAY";
+      break;
+    case "month":
+      startDate = subDays(now, 30);
+      precision = "DAY";
+      break;
+    case "year":
+      startDate = subDays(now, 365);
+      precision = "MONTH";
+      break;
+    case "all":
+    default:
+      precision = hasMultipleYears ? "YEAR" : "MONTH";
+  }
+
+  return startDate
+    ? {
+        dateRange: { startDate: formatISO(startDate) },
+        precision: precision as AnalyticsPrecision,
+      }
+    : {
+        dateRange: {},
+        precision: precision as AnalyticsPrecision,
+      };
+};
+
 export const chartSettingsReducer = (state: State, action: Action) => {
-  /* eslint-disable no-case-declarations */
+  /* eslint-disable no-var */
   switch (action.type) {
     case "dateRange":
+      var dateVars = getDateVars(state.minDate, action.value);
+      return { ...state, ...dateVars, dateLabel: action.value, updated: true };
+
     case "chart":
-      const now = Date.now();
-
-      const hasMultipleYears = state.minDate
-        ? differenceInCalendarYears(parseISO(state.minDate), now) > 0
-        : true;
-
-      let startDate;
-      let precision;
-      switch (action.value) {
-        case "week":
-          startDate = subDays(now, 7);
-          precision = "DAY";
-          break;
-        case "month":
-          startDate = subDays(now, 30);
-          precision = "DAY";
-          break;
-        case "year":
-          startDate = subDays(now, 365);
-          precision = "MONTH";
-          break;
-        case "all":
-        default:
-          precision = hasMultipleYears ? "YEAR" : "MONTH";
-      }
-
-      const dateVars = startDate
-        ? {
-            dateRange: { startDate: formatISO(startDate) },
-            dateLabel: action.type === "chart" ? state.dateLabel : action.value,
-            precision: precision as AnalyticsPrecision,
-          }
-        : {
-            dateRange: {},
-            dateLabel: action.type === "chart" ? state.dateLabel : action.value,
-            precision: precision as AnalyticsPrecision,
-          };
-
-      const chartType =
-        action.type === "chart" ? action.value : state.chartType;
-
-      return { ...state, ...dateVars, chartType, updated: true };
+      var dateVars = getDateVars(state.minDate, state.dateLabel);
+      return { ...state, ...dateVars, chartType: action.value, updated: true };
 
     case "region":
-      const usOnly = action.value === "US";
-      return { ...state, usOnly, updated: true };
+      return { ...state, usOnly: action.value === "US", updated: true };
 
     default:
       return state;
