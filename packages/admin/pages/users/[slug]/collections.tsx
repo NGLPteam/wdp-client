d5c8c3dcdf0fb44d3793ba6e4547c1602ce72652
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2024-03-05 12:24:17 -0800

    [C] Switch user subpages to transition wrapper

--- packages/admin/pages/users/[slug]/collections.tsx
+++ packages/admin/pages/users/[slug]/collections.tsx
@@ -1,5 +1,5 @@
 import { graphql, usePreloadedQuery, PreloadedQuery } from "react-relay";
-import { QueryLoaderWrapper } from "@wdp/lib/api/components";
+import { QueryTransitionWrapper } from "@wdp/lib/api/components";
 import UserCollectionsList from "components/composed/user/UserCollectionsList";
 import type { collectionsManageSlugUsersPagesQuery as Query } from "@/relay/collectionsManageSlugUsersPagesQuery.graphql";
 import type { GetLayout } from "@wdp/lib/types/page";
@@ -27,7 +27,7 @@ const getLayout: GetLayout<Props> = (props) => {
   const { PageComponent, pageComponentProps } = props;
 
   return (
-    <QueryLoaderWrapper<Query>
+    <QueryTransitionWrapper<Query>
       query={query}
       variables={{ ...queryVars, userSlug }}
       loadingFallback={<LoadingCircle />}
@@ -42,7 +42,7 @@ const getLayout: GetLayout<Props> = (props) => {
           />
         )
       }
-    </QueryLoaderWrapper>
+    </QueryTransitionWrapper>
   );
 };
 UserCollections.getLayout = getLayout;
--- packages/admin/pages/users/[slug]/items.tsx
+++ packages/admin/pages/users/[slug]/items.tsx
@@ -1,5 +1,5 @@
 import { graphql, usePreloadedQuery, PreloadedQuery } from "react-relay";
-import { QueryLoaderWrapper } from "@wdp/lib/api/components";
+import { QueryTransitionWrapper } from "@wdp/lib/api/components";
 import UserItemsList from "components/composed/user/UserItemsList";
 import type { itemsManageSlugUsersPagesQuery as Query } from "@/relay/itemsManageSlugUsersPagesQuery.graphql";
 import type { GetLayout } from "@wdp/lib/types/page";
@@ -27,7 +27,7 @@ const getLayout: GetLayout<Props> = (props) => {
   const { PageComponent, pageComponentProps } = props;
 
   return (
-    <QueryLoaderWrapper<Query>
+    <QueryTransitionWrapper<Query>
       query={query}
       variables={{ ...queryVars, userSlug }}
       loadingFallback={<LoadingCircle />}
@@ -42,7 +42,7 @@ const getLayout: GetLayout<Props> = (props) => {
           />
         )
       }
-    </QueryLoaderWrapper>
+    </QueryTransitionWrapper>
   );
 };
 UserItems.getLayout = getLayout;
