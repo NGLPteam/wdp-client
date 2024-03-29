82ad7c940eef64cc561c2f84ecdb5a4a9b31433d
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2024-03-29 11:04:13 -0700

    [C] Cleanup unused code; run depcheck

--- packages/frontend/contexts/index.ts
+++
@@ -1,5 +0,0 @@
-export {
-  QueryStateContext,
-  QueryVariablesContext,
-} from "@wdp/lib/api/contexts";
-export { default as useViewerContext } from "./useViewerContext";
--- packages/frontend/routes/RouteHelper.ts
+++
@@ -1,4 +0,0 @@
-import NextNamedRoutes from "@wdp/lib/routes/helpers/NextNamedRoutes";
-import { baseRoutes } from "./baseRoutes";
-
-export const RouteHelper = new NextNamedRoutes(baseRoutes);
