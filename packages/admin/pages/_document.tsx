5168958009bb5622b20adb29d451732451e13bb3
Jen Wardwell <jen@castironcoding.com>
2024-06-28 16:03:49 -0700

    [F] Fix preloaded font file reference - Resolves MERU-867

--- packages/admin/pages/_document.tsx
+++ packages/admin/pages/_document.tsx
@@ -90,7 +90,7 @@ export default class AppDocument extends Document<Props> {
             <React.Fragment key={font}>
               <link
                 rel="preload"
-                href={`fonts/${font}.woff2`}
+                href={`/fonts/${font}.woff2`}
                 as="font"
                 type="font/woff2"
                 crossOrigin="anonymous"
