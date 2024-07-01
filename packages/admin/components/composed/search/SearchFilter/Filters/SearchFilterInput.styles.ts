ae453ab0730049bfb69ccc166f9a185dd0ecb842
Jen Wardwell <jen@castironcoding.com>
2024-07-01 13:07:37 -0700

    [B] Fix search filter flex wrapper on mobile devices - Resolves MERU-843

--- packages/admin/components/composed/search/SearchFilter/Filters/SearchFilterInput.styles.ts
+++ packages/admin/components/composed/search/SearchFilter/Filters/SearchFilterInput.styles.ts
@@ -3,5 +3,10 @@ import { pxToRem } from "@wdp/lib/theme/functions";
 
 export const FlexWrapper = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: ${pxToRem(24)};
+
+  > * {
+    flex: 1 1 auto;
+  }
 `;
