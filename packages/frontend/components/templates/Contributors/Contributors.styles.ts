3dec63229dbf75ec215e0d17bcdc07957725474b
1aurend <imlcdavidson@gmail.com>
2024-10-22 10:14:10 -0700

    [F] Scaffold initial contributors template

---
+++ packages/frontend/components/templates/Contributors/Contributors.styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { pxToRem } from "@wdp/lib/theme/functions";
+import { lGrid } from "theme/mixins";
+
+export const SectionInner = styled.div<{ $paddingBottom?: string }>`
+  padding-block-start: var(--container-padding-md);
+  padding-block-end: var(
+    --container-padding-${({ $paddingBottom = "md" }) => $paddingBottom}
+  );
+`;
+
+export const List = styled.ul`
+  ${lGrid()}
+  row-gap: ${pxToRem(30)};
+  padding-block-start: var(--padding-xl);
+`;
