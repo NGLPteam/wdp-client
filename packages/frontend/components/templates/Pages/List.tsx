c5a53db0965da95ee42a07f6b81e4c03061c8de7
1aurend <imlcdavidson@gmail.com>
2024-11-13 08:43:49 -0800

    [F] Wire up pages template

--- packages/frontend/components/templates/Pages/List.tsx
+++ packages/frontend/components/templates/Pages/List.tsx
@@ -1,25 +1,31 @@
+import { graphql, useFragment } from "react-relay";
 import { useRouteSlug } from "@wdp/lib/routes";
 import { NamedLink } from "@/components/atomic";
+import { ListPagesTemplateFragment$key } from "@/relay/ListPagesTemplateFragment.graphql";
 import * as Styled from "./Pages.styles";
 
-type Page = {
-  slug?: string | null;
-  title?: string | null;
-};
-
-export default function List() {
+export default function List({
+  data,
+}: {
+  data?: ListPagesTemplateFragment$key | null;
+}) {
   const slug = useRouteSlug();
 
-  const pages: Page[] = [];
+  const { pages } = useFragment(fragment, data) ?? {};
+
+  const canRender =
+    !!pages?.edges?.length && pages.edges.some((e) => !!e.node.slug);
+
+  if (!canRender) return null;
 
   return (
     slug && (
       <Styled.List>
-        {pages.map((p) =>
-          p.slug ? (
-            <Styled.ListItem key={p.slug}>
-              <NamedLink href={`/collections/${slug}/page/${p.slug}}`}>
-                <span className="t-label-sm t-copy-light">{p.title}</span>
+        {pages.edges.map((p) =>
+          p.node.slug ? (
+            <Styled.ListItem key={p.node.slug}>
+              <NamedLink href={`/collections/${slug}/page/${p.node.slug}}`}>
+                <span className="t-label-sm t-copy-light">{p.node.title}</span>
               </NamedLink>
             </Styled.ListItem>
           ) : null,
@@ -29,15 +35,37 @@ export default function List() {
   );
 }
 
-// const fragment = graphql`
-//   fragment IssueSidebarNavListFragment on Collection {
-//     pages {
-//       edges {
-//         node {
-//           slug
-//           title
-//         }
-//       }
-//     }
-//   }
-// `;
+const fragment = graphql`
+  fragment ListPagesTemplateFragment on AnyEntity {
+    ... on Item {
+      pages {
+        edges {
+          node {
+            slug
+            title
+          }
+        }
+      }
+    }
+    ... on Collection {
+      pages {
+        edges {
+          node {
+            slug
+            title
+          }
+        }
+      }
+    }
+    ... on Community {
+      pages {
+        edges {
+          node {
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`;
