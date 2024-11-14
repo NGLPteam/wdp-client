47e77453052811479e4ef78260c8ce427c7fc591
1aurend <imlcdavidson@gmail.com>
2024-11-14 08:59:10 -0800

    [F] Refactor entity metadata to render templates

--- packages/frontend/app/(frontend)/(pages)/templates/items/[slug]/page.tsx
+++ packages/frontend/app/(frontend)/(pages)/templates/items/[slug]/page.tsx
@@ -1,8 +1,6 @@
-import { useId } from "react";
 import { graphql } from "relay-runtime";
 import { notFound } from "next/navigation";
 import TemplateFactory from "@/components/templates/Factory";
-import NavigationTemplate from "@/components/templates/EntityNavigation";
 import fetchQuery from "@/lib/relay/fetchQuery";
 import { pageItemTemplateQuery as Query } from "@/relay/pageItemTemplateQuery.graphql";
 import UpdateClientEnvironment from "@/lib/relay/UpdateClientEnvironment";
@@ -11,8 +9,6 @@ import { BasePageParams } from "@/types/page";
 export default async function TemplatePage({
   params: { slug },
 }: BasePageParams) {
-  const uid = useId();
-
   const { data, records } =
     (await fetchQuery<Query>(query, {
       slug,
@@ -22,13 +18,12 @@ export default async function TemplatePage({
 
   if (!item) return notFound();
 
-  const { navigation, main } = item.layouts;
+  const { main } = item.layouts;
 
   const { templates } = main ?? {};
 
   return (
     <UpdateClientEnvironment records={records}>
-      <NavigationTemplate data={navigation} skipToId={`${uid}-tab-content`} />
       {!!templates?.length &&
         templates.map((t, i) => <TemplateFactory key={i} data={t} />)}
     </UpdateClientEnvironment>
@@ -39,9 +34,6 @@ const query = graphql`
   query pageItemTemplateQuery($slug: Slug!) {
     item(slug: $slug) {
       layouts {
-        navigation {
-          ...EntityNavigationTemplateFragment
-        }
         main {
           templates {
             ...FactoryTemplatesFragment
