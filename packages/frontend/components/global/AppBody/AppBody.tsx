1192fb6158863c4123ba1754450e588e581fcf65
1aurend <imlcdavidson@gmail.com>
2024-11-22 13:49:16 -0800

    [B][E] Remove search modal portal

--- packages/frontend/components/global/AppBody/AppBody.tsx
+++ packages/frontend/components/global/AppBody/AppBody.tsx
@@ -4,6 +4,7 @@ import { graphql } from "react-relay";
 import { updateI18n } from "i18n";
 import { useMaybeFragment } from "@wdp/lib/api/hooks";
 import { AppBodyFragment$key } from "@/relay/AppBodyFragment.graphql";
+import { SearchButtonFragment$key } from "@/relay/SearchButtonFragment.graphql";
 import AppHeader from "../AppHeader";
 import AppFooter from "../AppFooter";
 import * as Styled from "./AppBody.styles";
@@ -11,16 +12,17 @@ import * as Styled from "./AppBody.styles";
 interface Props {
   children: React.ReactNode;
   data?: AppBodyFragment$key | null;
+  searchData?: SearchButtonFragment$key | null;
 }
 
-function AppBody({ children, data }: Props) {
+function AppBody({ children, data, searchData }: Props) {
   updateI18n("en");
 
   const globalData = useMaybeFragment(fragment, data);
 
   return (
     <Styled.Body className="a-bg-neutral00">
-      <AppHeader data={globalData} />
+      <AppHeader data={globalData} searchData={searchData} />
       <Styled.Main id="main" tabIndex={-1}>
         {children}
       </Styled.Main>
