d72574cf2b69fa9d71a69c1183b4bbe098d5c497
1aurend <imlcdavidson@gmail.com>
2024-11-25 14:30:48 -0800

    [C] Migrate links, Contributors template to css modules

--- packages/frontend/components/atomic/images/Avatar/Avatar.tsx
+++ packages/frontend/components/atomic/images/Avatar/Avatar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/legacy/image";
 import { pxToRem } from "@wdp/lib/theme/functions";
 import { IconFactory } from "components/factories";
-import * as Styled from "./Avatar.styles";
+import styles from "./Avatar.module.css";
 
 const Avatar = ({ url, alt, size = "sm" }: Props) => {
   const imageSize = size === "sm" ? 32 : 60;
@@ -13,7 +13,7 @@ const Avatar = ({ url, alt, size = "sm" }: Props) => {
   } as React.CSSProperties;
 
   return (
-    <Styled.Wrapper style={style}>
+    <div className={styles.wrapper} style={style}>
       {url && (
         <Image src={url} alt={alt || ""} width={imageSize} height={imageSize} />
       )}
@@ -22,7 +22,7 @@ const Avatar = ({ url, alt, size = "sm" }: Props) => {
         aria-hidden="true"
         role="presentation"
       />
-    </Styled.Wrapper>
+    </div>
   );
 };
 
