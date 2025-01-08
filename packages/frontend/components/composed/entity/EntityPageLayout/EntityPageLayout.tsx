33b5a2a9d1a5f85baa5fcf64c0939c222e36860d
1aurend <imlcdavidson@gmail.com>
2025-01-08 12:57:15 -0800

    [C] Migrate composed/entity

--- packages/frontend/components/composed/entity/EntityPageLayout/EntityPageLayout.tsx
+++ packages/frontend/components/composed/entity/EntityPageLayout/EntityPageLayout.tsx
@@ -1,31 +1,33 @@
 "use client";
 
-import { useMaybeFragment } from "@wdp/lib/api/hooks";
+import classNames from "classnames";
 import useIsMounted from "@wdp/lib/hooks/useIsMounted";
-import { graphql } from "react-relay";
+import { graphql, useFragment } from "react-relay";
 import { ContentImage, Markdown, LoadingBlock } from "components/atomic";
 import { EntityPageLayoutFragment$key } from "@/relay/EntityPageLayoutFragment.graphql";
-import * as Styled from "./EntityPageLayout.styles";
+import styles from "./EntityPageLayout.module.css";
 
 export default function EntityPageLayout({ data }: Props) {
-  const page = useMaybeFragment(fragment, data);
+  const page = useFragment(fragment, data);
 
   const isMounted = useIsMounted();
 
   return page ? (
-    <Styled.Wrapper className="l-container-wide a-bg-neutral00">
-      <Styled.Inner className="t-rte">
+    <section
+      className={classNames(styles.wrapper, "l-container-wide a-bg-neutral00")}
+    >
+      <div className={classNames("t-rte", styles.inner)}>
         {page.heroImage && <ContentImage data={page.heroImage} />}
         <h2 className="t-h3">{page.title}</h2>
         {isMounted ? (
           <Markdown.Page>{page.body}</Markdown.Page>
         ) : (
-          <Styled.LoadingWrapper className="t-rte">
+          <div className={classNames("t-rte", styles.loading)}>
             <LoadingBlock />
-          </Styled.LoadingWrapper>
+          </div>
         )}
-      </Styled.Inner>
-    </Styled.Wrapper>
+      </div>
+    </section>
   ) : null;
 }
 
