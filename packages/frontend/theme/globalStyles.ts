2d0701d926e23c2216c4295eee7b0396a3589915
1aurend <imlcdavidson@gmail.com>
2025-01-09 11:26:49 -0800

    [C] Migrate root

--- packages/frontend/theme/globalStyles.ts
+++ packages/frontend/theme/globalStyles.ts
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 import { baseResets } from "@wdp/lib/theme";
-import baseRoot from "./base/root";
 import baseFonts from "./base/fonts";
 import baseAppearance from "./base/appearance";
 import baseTypography from "./base/typography";
@@ -8,7 +7,6 @@ import baseTypography from "./base/typography";
 const GlobalStyles = createGlobalStyle`
   @layer styled-components {
     ${baseFonts}
-    ${baseRoot}
     ${baseResets}
     ${baseAppearance}
     ${baseTypography}
