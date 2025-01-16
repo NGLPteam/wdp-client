6f5574adfdc5be536eeae65dedbbeb44ffebe834
1aurend <imlcdavidson@gmail.com>
2025-01-16 10:18:46 -0800

    [C] Remove a few additional unused components

--- packages/frontend/components/atomic/properties/index.ts
+++ packages/frontend/components/atomic/properties/index.ts
@@ -1,10 +1,5 @@
-export { default as CCLicense } from "./CCLicense";
 export { default as DOI } from "./DOI";
-export { default as FullText } from "./FullText";
 export { default as ISSN } from "./ISSN";
-export { default as OpenAccess } from "./OpenAccess";
-export { default as PeerReviewed } from "./PeerReviewed";
 export { default as PrecisionDate } from "./PrecisionDate";
-export { default as PreprintVersion } from "./PreprintVersion";
 export { default as ViewCount } from "./ViewCount";
 export { default as DownloadCount } from "./DownloadCount";
