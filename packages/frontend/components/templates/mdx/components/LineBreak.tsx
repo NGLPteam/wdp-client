fafed85205edc745c66d4fe7e9ef258275f15b12
Alexa Grey <alexa@castironcoding.com>
2025-01-16 13:52:52 -0800

    [C] Add LineBreak MDX tag

---
+++ packages/frontend/components/templates/mdx/components/LineBreak.tsx
@@ -0,0 +1,8 @@
+/**
+ * This tag operates as a simple wrapper around `<br/>` tags,
+ * which are difficult to produce properly from the API during
+ * its sanitization process.
+ */
+export default function LineBreak() {
+  return <br/>;
+}
