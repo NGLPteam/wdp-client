2024d58c349ab6cb43d0464f86ba8d4dea3c47f8
1aurend <imlcdavidson@gmail.com>
2025-02-13 11:22:39 -0800

    [E] Improve labeling of page slug field

--- packages/admin/components/composed/pages/EntityPageAddForm/EntityPageAddForm.tsx
+++ packages/admin/components/composed/pages/EntityPageAddForm/EntityPageAddForm.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import { useFragment, graphql } from "react-relay";
 import MutationForm, {
   useRenderForm,
@@ -18,8 +17,6 @@ export default function EntityPageAddForm({
   onSuccess,
   onCancel,
 }: Props) {
-  const slugRef = useRef<HTMLInputElement | null>(null);
-
   const sourceEntity = useFragment<EntityPageAddFormFragment$key>(
     fragment,
     data,
@@ -39,35 +36,17 @@ export default function EntityPageAddForm({
   );
 
   const renderForm = useRenderForm<Fields>(({ form: { register } }) => {
-    const { onChange, ...registerTitle } = register("title");
-    const { ref, ...registerSlug } = register("slug");
-
-    // Set the slug field's default value when the title changes
-    // Setting default value allows the user to change the slug if they need to
-    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (slugRef.current)
-        slugRef.current.defaultValue = convertToSlug(e.target.value);
-    };
-
     return (
       <Forms.Grid>
         <Forms.Input
           label="forms.fields.title"
           required
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            onChange(e);
-            handleTitleChange(e);
-          }}
-          {...registerTitle}
+          {...register("title")}
         />
         <Forms.Slug
           label="forms.fields.slug"
-          required
-          ref={(e) => {
-            ref(e);
-            slugRef.current = e;
-          }}
-          {...registerSlug}
+          description="forms.fields.page_slug_description"
+          {...register("slug")}
         />
         <Forms.FileImageUpload
           label="forms.fields.hero_image"
