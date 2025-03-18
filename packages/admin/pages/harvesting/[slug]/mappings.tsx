3b3d16d82b69d11e54b28468d2b6899a4e0f91fe
1aurend <imlcdavidson@gmail.com>
2025-03-18 13:41:44 -0700

    [E] Consolidate harvesting pages layout

--- packages/admin/pages/harvesting/[slug]/mappings.tsx
+++ packages/admin/pages/harvesting/[slug]/mappings.tsx
@@ -1,12 +1,9 @@
 import { graphql, usePreloadedQuery, PreloadedQuery } from "react-relay";
-import { QueryTransitionWrapper } from "@wdp/lib/api/components";
-import { LoadingPage } from "components/atomic";
-import { useRouteSlug, useBaseListQueryVars, useSearchQueryVars } from "hooks";
 import HarvestSourceLayout from "components/composed/harvesting/HarvestSourceLayout";
 import HarvestMappingsList from "components/composed/harvesting/HarvestMappingsList";
-import ErrorPage from "next/error";
-import { LoadingCircle } from "components/atomic";
+import { useSearchQueryVars, useBaseListQueryVars } from "hooks";
 import type { mappingsHarvestSourceQuery as Query } from "@/relay/mappingsHarvestSourceQuery.graphql";
+import Layout from "./layout";
 import type { GetLayout } from "@wdp/lib/types/page";
 
 function HarvestSourceMappings({ queryRef, ...layoutProps }: Props) {
@@ -20,33 +17,12 @@ function HarvestSourceMappings({ queryRef, ...layoutProps }: Props) {
 }
 
 const getLayout: GetLayout<Props> = (props) => {
-  const queryVars = useBaseListQueryVars();
-  const slug = useRouteSlug();
-  const _searchVars = useSearchQueryVars();
+  useBaseListQueryVars();
+  useSearchQueryVars();
 
-  if (!slug) return <ErrorPage statusCode={404} />;
-
-  const { PageComponent, pageComponentProps } = props;
-
-  return (
-    <QueryTransitionWrapper<Query>
-      query={query}
-      variables={{ ...queryVars, slug }}
-      loadingFallback={<LoadingPage />}
-      refetchTags={["harvestMappings"]}
-    >
-      {({ queryRef }) =>
-        queryRef ? (
-          <PageComponent {...pageComponentProps} queryRef={queryRef} />
-        ) : (
-          <HarvestSourceLayout>
-            <LoadingCircle className="l-page-loading" />
-          </HarvestSourceLayout>
-        )
-      }
-    </QueryTransitionWrapper>
-  );
+  return <Layout query={query} {...props} />;
 };
+
 HarvestSourceMappings.getLayout = getLayout;
 
 export default HarvestSourceMappings;
