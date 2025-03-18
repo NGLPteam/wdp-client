67604f976e40115c19a1220afbcbda2b088ff4e9
1aurend <imlcdavidson@gmail.com>
2025-03-18 16:51:33 -0700

    [F] Update extraction mapping template input for harvest mappings

--- packages/admin/components/forms/ExtractionMappingTemplateInput/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import { pxToRem } from "theme/mixins/functions";
-import ButtonControl from "components/atomic/buttons/ButtonControl";
-
-export const Reset = styled(ButtonControl)`
-  margin-block-start: ${pxToRem(8)};
-`;
