513ab962e9d2d5593fda5805ee150f64d1648648
1aurend <imlcdavidson@gmail.com>
2025-03-19 15:50:21 -0700

    [B] Small style adjustments

--- packages/admin/components/composed/model/ModelColumns/StringColumn.tsx
+++ packages/admin/components/composed/model/ModelColumns/StringColumn.tsx
@@ -7,11 +7,13 @@ type Props<T extends Node> = PartialColumnish<T> & {
   header: () => string | React.ReactNode;
   id: string;
   capitalize?: boolean;
+  cellType?: "url" | "name";
 };
 
 const StringColumn = <T extends Node>({
   header,
   id,
+  cellType,
   ...props
 }: Props<T>): ColumnDef<T> => {
   const { accessorKey } = getAccessorProps<T>(props);
@@ -23,6 +25,9 @@ const StringColumn = <T extends Node>({
       ? { accessorKey }
       : { accessorFn: (originalRow: T) => get(originalRow, id) }),
     enableSorting: false,
+    meta: {
+      cellType,
+    },
     ...props,
   };
 };
