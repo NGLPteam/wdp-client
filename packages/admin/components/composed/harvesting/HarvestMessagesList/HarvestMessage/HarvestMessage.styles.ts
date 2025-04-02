29dae4531f76f90b973114ef45ec4086fcb775b5
1aurend <imlcdavidson@gmail.com>
2025-04-01 17:14:26 -0700

    [E] Wrap up messages display

--- packages/admin/components/composed/harvesting/HarvestMessagesList/HarvestMessage/HarvestMessage.styles.ts
+++ packages/admin/components/composed/harvesting/HarvestMessagesList/HarvestMessage/HarvestMessage.styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { pxToRem } from "theme/mixins/functions";
 import { tLabel } from "theme/mixins/typography";
+import { respond } from "theme/mixins/base";
 
 export const Grid = styled.li`
   display: grid;
@@ -9,6 +10,8 @@ export const Grid = styled.li`
   border-top: 1px solid var(--color-lighter);
   padding-block: 1rem;
   border-color: var(--color-light);
+
+  ${respond(`grid-template-columns: 1fr; gap: 1rem;`, 80)}
 `;
 
 export const Date = styled.div``;
@@ -23,6 +26,15 @@ export const Level = styled.div`
   align-self: start;
 `;
 
+export const Tag = styled.span`
+  color: var(--brand100);
+  padding: 0.15rem 0.35rem;
+  background: var(--brand10);
+  width: max-content;
+  border-radius: ${pxToRem(4)};
+  ${tLabel("sm")}
+`;
+
 export const Body = styled.div`
   > * + * {
     margin-block-start: 1rem;
@@ -34,4 +46,9 @@ export const Links = styled.div`
   gap: ${pxToRem(30)};
   color: var(--brand100);
   font-weight: var(--font-weight-medium);
+`;
+
+export const Tags = styled.div`
+  display: flex;
+  gap: ${pxToRem(12)};
 `;
