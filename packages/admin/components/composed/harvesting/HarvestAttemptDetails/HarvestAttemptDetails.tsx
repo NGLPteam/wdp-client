198f3071f1331cb233d341b041467654aa51b736
1aurend <imlcdavidson@gmail.com>
2025-04-02 13:40:48 -0700

    [F] Add details view for attempts

---
+++ packages/admin/components/composed/harvesting/HarvestAttemptDetails/HarvestAttemptDetails.tsx
@@ -0,0 +1,77 @@
+import { useFragment, graphql } from "react-relay";
+import dynamic from "next/dynamic";
+import { Forms } from "components/api/MutationForm";
+import { useTranslation } from "react-i18next";
+import { formatDate } from "@wdp/lib/helpers";
+import MockInput from "components/forms/MockInput";
+import { FormFieldSkeleton } from "components/atomic/loading";
+import type { HarvestAttemptDetailsFragment$key } from "@/relay/HarvestAttemptDetailsFragment.graphql";
+
+const CodeEditor = dynamic(() => import("components/forms/CodeEditor"), {
+  loading: () => <FormFieldSkeleton />,
+  ssr: false,
+});
+
+export default function HarvestAttemptDetails({
+  data,
+}: {
+  data: HarvestAttemptDetailsFragment$key;
+}) {
+  const { t } = useTranslation();
+
+  const fields = useFragment<HarvestAttemptDetailsFragment$key>(fragment, data);
+
+  return (
+    <Forms.Grid>
+      <MockInput label="lists.status_column" value={fields.currentState} />
+      <MockInput label="lists.record_count_column" value={fields.recordCount} />
+      <MockInput
+        label="lists.began_at_column"
+        value={formatDate(fields.beganAt ?? "")}
+      />
+      <MockInput
+        label="lists.ended_at_column"
+        value={formatDate(fields.beganAt ?? "")}
+      />
+      <MockInput
+        label="forms.fields.metadata_format"
+        value={fields.metadataFormat}
+        isWide={fields.mode === "SCHEDULED"}
+      />
+      <MockInput label="harvesting.mode" value={fields.mode} />
+      {fields.mode === "SCHEDULED" && (
+        <MockInput
+          label="harvesting.current_schedule"
+          value={fields.scheduledAt ?? ""}
+        />
+      )}
+      <MockInput label="forms.fields.note" value={fields.note} isWide />
+      <MockInput
+        label="forms.extraction_mapping_template.label"
+        value={() => (
+          <CodeEditor
+            unwrapped
+            label={t("forms.extraction_mapping_template.code_editor_label")}
+            value={fields.extractionMappingTemplate ?? ""}
+          />
+        )}
+        isWide
+      />
+    </Forms.Grid>
+  );
+}
+
+const fragment = graphql`
+  fragment HarvestAttemptDetailsFragment on HarvestAttempt {
+    id
+    currentState
+    metadataFormat
+    beganAt
+    endedAt
+    mode
+    scheduledAt
+    recordCount
+    note
+    extractionMappingTemplate
+  }
+`;
---
+++ packages/admin/components/forms/MockInput/MockInput.tsx
@@ -0,0 +1,53 @@
+import { useId } from "react";
+import { useTranslation } from "react-i18next";
+import BaseInputLabel from "components/forms/BaseInputLabel";
+import * as Styled from "./MockInput.styles";
+
+export default function MockInput({
+  description,
+  descriptionTop,
+  hideLabel,
+  label,
+  isWide,
+  value,
+}: Props) {
+  const { t } = useTranslation();
+  const uid = useId();
+  // Setting the width this way creates a small extra class that's applied for wide wrappers
+  // This prevents the Styled.Wrapper CSS from getting duplicated for a single width change
+  const css = isWide && {
+    "flex-basis": "var(--form-grid-item-width-wide)",
+  };
+
+  return (
+    <Styled.Wrapper css={css}>
+      <BaseInputLabel htmlFor={uid} $hideLabel={hideLabel}>
+        {t(label)}
+      </BaseInputLabel>
+      {description && descriptionTop && (
+        <Styled.Description $descriptionTop={descriptionTop}>
+          {typeof description === "string" ? t(description) : description}
+        </Styled.Description>
+      )}
+      {typeof value === "function" ? (
+        value()
+      ) : (
+        <Styled.Input id={uid}>{value}</Styled.Input>
+      )}
+      {description && !descriptionTop && (
+        <Styled.Description>
+          {typeof description === "string" ? t(description) : description}
+        </Styled.Description>
+      )}
+    </Styled.Wrapper>
+  );
+}
+
+interface Props {
+  isWide?: boolean;
+  descriptionTop?: boolean;
+  description?: string;
+  hideLabel?: boolean;
+  label: string;
+  value?: string | number | (() => React.JSX.Element) | null;
+}
