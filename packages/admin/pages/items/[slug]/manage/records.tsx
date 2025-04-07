2d54b93119505c6ebada04624b9a4d6d73c1e59a
1aurend <imlcdavidson@gmail.com>
2025-04-07 14:50:03 -0700

    [C] Refine item layout + page components

--- packages/admin/pages/items/[slug]/manage/records.tsx
+++ packages/admin/pages/items/[slug]/manage/records.tsx
@@ -1,52 +1,25 @@
 import { graphql, usePreloadedQuery, PreloadedQuery } from "react-relay";
-import { useRouteSlug } from "hooks";
-import ErrorPage from "next/error";
-import { LoadingPage } from "components/atomic";
-import ItemLayout from "components/composed/item/ItemLayout";
 import EntityHarvestRecordsList from "components/composed/harvesting/EntityHarvestRecordsList";
-import { AuthContextProvider } from "contexts/AuthContext";
-import { QueryTransitionWrapper } from "@wdp/lib/api/components";
 import type { recordsItemQuery as Query } from "@/relay/recordsItemQuery.graphql";
+import Layout from "./_layout";
 import type { GetLayout } from "@wdp/lib/types/page";
 
-function ItemRecords({ queryRef, ...layoutProps }: Props) {
+function ItemRecords({ queryRef }: Props) {
   const { item } = usePreloadedQuery<Query>(query, queryRef);
 
   return item ? (
-    <AuthContextProvider data={item}>
-      <ItemLayout data={item} {...layoutProps}>
-        <EntityHarvestRecordsList data={item} headerStyle="secondary" />
-      </ItemLayout>
-    </AuthContextProvider>
+    <EntityHarvestRecordsList data={item} headerStyle="secondary" />
   ) : null;
 }
 
-const getLayout: GetLayout<Props> = (props) => {
-  const slug = useRouteSlug();
-
-  if (!slug) return <ErrorPage statusCode={404} />;
-
-  const { PageComponent, pageComponentProps } = props;
-
-  return (
-    <QueryTransitionWrapper<Query>
-      query={query}
-      variables={{ slug }}
-      loadingFallback={<LoadingPage />}
-    >
-      {({ queryRef }) =>
-        queryRef && (
-          <PageComponent
-            {...pageComponentProps}
-            queryRef={queryRef}
-            showSidebar
-            useRouteHeader={false}
-          />
-        )
-      }
-    </QueryTransitionWrapper>
-  );
-};
+const getLayout: GetLayout<Props> = (props) => (
+  <Layout
+    query={query}
+    refetchTags={["harvestRecords"]}
+    modelName="harvest_record"
+    {...props}
+  />
+);
 
 ItemRecords.getLayout = getLayout;
 
@@ -54,16 +27,12 @@ export default ItemRecords;
 
 type Props = {
   queryRef: PreloadedQuery<Query>;
-  showSidebar: true;
-  useRouteHeader: false;
 };
 
 const query = graphql`
   query recordsItemQuery($slug: Slug!) {
     item(slug: $slug) {
       ...EntityHarvestRecordsListFragment
-      ...ItemLayoutFragment
-      ...AuthContextFragment
     }
   }
 `;
