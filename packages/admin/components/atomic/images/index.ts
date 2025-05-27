e1b22fcddf7a307439ae061e8bbe16a89a14a2ea
1aurend <imlcdavidson@gmail.com>
2025-05-27 11:04:50 -0700

    [C] Remove next/image from admin app

--- packages/admin/components/atomic/images/index.ts
+++ packages/admin/components/atomic/images/index.ts
@@ -1,4 +1,3 @@
 export { default as Avatar } from "./Avatar";
-export { default as BaseImage } from "./BaseImage";
 export { default as CoverImage } from "./CoverImage";
 export { default as Image } from "./Image";
