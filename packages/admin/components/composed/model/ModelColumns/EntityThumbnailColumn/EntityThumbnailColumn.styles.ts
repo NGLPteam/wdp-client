5a1adba433d1a4885c7abc97eb643309f1c75d12
1aurend <imlcdavidson@gmail.com>
2025-06-04 16:26:12 -0700

    [B] Add spacing to thumbs in list

--- packages/admin/components/composed/model/ModelColumns/EntityThumbnailColumn/EntityThumbnailColumn.styles.ts
+++ packages/admin/components/composed/model/ModelColumns/EntityThumbnailColumn/EntityThumbnailColumn.styles.ts
@@ -3,12 +3,14 @@ import styled from "styled-components";
 
 export const Thumbnail = styled(Image)`
   object-position: bottom left;
+  max-width: 50px;
+  min-height: 50px;
+  max-height: 70px;
+  margin-block-end: 8px;
 
   table & {
     object-position: top right;
-    min-height: 50px;
-    max-width: 50px;
-    max-height: 70px;
+    margin-block-end: 0;
   }
 
   [data-mobile-layout="true"] & {
