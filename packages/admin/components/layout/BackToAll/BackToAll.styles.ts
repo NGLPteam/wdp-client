b3ad9241b53f2610ed8996c9bd7fed221d0c1fc9
1aurend <imlcdavidson@gmail.com>
2025-07-30 12:03:02 -0700

    [B] Fix harvesting breadcrumbs layout

--- packages/admin/components/layout/BackToAll/BackToAll.styles.ts
+++ packages/admin/components/layout/BackToAll/BackToAll.styles.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
+import { respond, fluidScale } from "theme/mixins/base";
 
 export const NavWrapper = styled.nav`
   padding-block-start: 24px;
   display: flex;
   align-items: center;
+  ${respond(`flex-wrap: wrap;`, "navBreak")}
 `;
 
-export const LinkWrapper = styled.span`
+export const LinkWrapper = styled.a`
   display: flex;
   align-items: center;
   font-size: var(--font-size-sm);
+  white-space: nowrap;
 
   > * + * {
     margin-inline-start: 5px;
@@ -18,4 +21,16 @@ export const LinkWrapper = styled.span`
   & + & {
     margin-inline-start: 5px;
   }
+`;
+
+export const Label = styled.span<{ $nested: boolean }>`
+  --Label-width-offset: ${fluidScale("60px", "20px", "1280px", "1024px")};
+
+  ${({ $nested }) =>
+    $nested &&
+    `max-width: calc(var(--container-max) / 2 - var(--Label-width-offset));`}
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  ${respond(`max-width: 85vw;`, "navBreak")}
 `;
