e43bc407b3d95910a154e60a787e646dcd0639ec
1aurend <imlcdavidson@gmail.com>
2025-09-09 13:19:05 -0700

    [B] Ensure session token passed to fetchQuery

--- packages/frontend/lib/relay/fetchQuery.ts
+++ packages/frontend/lib/relay/fetchQuery.ts
@@ -3,14 +3,17 @@ import {
   GraphQLTaggedNode,
   fetchQuery as relayFetch,
 } from "relay-runtime";
-import { default as getRelayEnvironment } from "@wdp/lib/app/buildEnvironment";
+import { auth } from "@/lib/auth/initAuth";
+import { getCurrentEnvironment } from "./environment";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default async function fetchQuery<Q extends OperationType>(
   query: GraphQLTaggedNode,
   vars: Record<string, any>,
 ) {
-  const env = getRelayEnvironment();
+  const session = await auth();
+
+  const env = getCurrentEnvironment({ sessionToken: session?.accessToken });
 
   let data;
 
