f36e880b4ac9f0ba6bced61248d2717f1fec7e66
1aurend <imlcdavidson@gmail.com>
2025-10-07 14:25:28 -0700

    [E] Add pressbooks to protocol + format options

--- packages/admin/components/composed/harvesting/constants.ts
+++ packages/admin/components/composed/harvesting/constants.ts
@@ -5,9 +5,13 @@ export const METADATA_FORMAT_OPTS = [
   { label: "METS", value: "METS" },
   { label: "MODS", value: "MODS" },
   { label: "OAIDC", value: "OAIDC" },
+  { label: "Pressbooks", value: "PRESSBOOKS" },
 ];
 
-export const PROTOCOL_FORMAT_OPTS = [{ label: "OAI-PMH", value: "OAI" }];
+export const PROTOCOL_FORMAT_OPTS = [
+  { label: "OAI-PMH", value: "OAI" },
+  { label: "Pressbooks", value: "PRESSBOOKS" },
+];
 
 export const METADATA_MAPPING_FIELD_OPTS = [
   { label: "", value: "" },
